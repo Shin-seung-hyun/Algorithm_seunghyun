fdd82986b1ca784f0bb55fcfd8198caedbe5dd34
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2022-12-22 15:34:37 +0900

    [level 2] Title: 기능개발, Time: 0.13 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -1,38 +1,40 @@
 import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         
-        int [] day = new int[progresses.length];
-        ArrayList<Integer> arr = new ArrayList<>();
+        int [] endDay = new int[speeds.length];
         
-        for(int i =0; i< progresses.length; i++){
-            day[i]=(100 - progresses[i])/speeds[i];
-            if ((100 - progresses[i]) % speeds[i] != 0){
-                day[i] += 1;
+        for(int i =0; i< speeds.length; i++ ){
+            
+            int day = (100 - progresses[i]) / speeds[i];
+            if( (100 - progresses[i]) % speeds[i] > 0 ){
+                day ++;
             }
+            endDay[i] = day;
         }
-
-        int pre = day[0];
-        int cnt = 1;
-        for(int i= 1; i< day.length; i++){
-            if(pre < day[i]) {
-                arr.add(cnt);
-                cnt = 1;
-                pre = day[i];
-            }
-            else {
-                cnt++;
-            }
-        }
-        arr.add(cnt);
         
-        int [] answer = new int[arr.size()];
-        for(int i =0; i< answer.length; i++){
-            answer[i] = arr.get(i);
+        Queue<Integer> queue = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i =0; i< endDay.length; i++){
+            
+            if( !queue.isEmpty() && queue.peek() < endDay[i]){
+                list.add(queue.size());
+                queue.clear();
+            }
+            queue.add(endDay[i]);
         }
+        list.add(queue.size());
+        
+        //ArrayList -> Array
+        int [] answer = new int [list.size()];
+        for(int i=0; i< list.size(); i++){
+            answer[i] = list.get(i).intValue();
+        }
+        
         return answer;
-        
-
     }
 }
