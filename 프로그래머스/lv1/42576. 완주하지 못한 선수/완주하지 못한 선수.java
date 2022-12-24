a422f7f7b3c8d7c0a7deb220bcc95b84cfcf60d2
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2022-12-24 16:46:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 300.73 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,18 +1,24 @@
 import java.util.Arrays;
 
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        String answer = "";
         
         Arrays.sort(participant);
         Arrays.sort(completion);
         
-        int i;
-        for( i=0; i<completion.length; i++)
-            if(!participant[i].equals(completion[i]))
-                break;
+        for(int i = 0; i< participant.length; i++){
+            if(i == completion.length) return participant[i];
+            
+            if(!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+            
 
-        return participant[i];
+        }
+        
+        return "";
     }
-
+    
 }
-
