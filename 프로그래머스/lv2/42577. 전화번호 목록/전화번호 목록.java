25feed8ea7e740c598904e61e036aa389b611f9f
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2022-12-27 16:29:33 +0900

    [level 2] Title: 전화번호 목록, Time: 212.58 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,16 +1,21 @@
-import java.util.Arrays;
+import java.util.HashSet;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-
-        Arrays.sort(phone_book);
         
-        for(int i = 0; i< phone_book.length-1; i++){
-            if(phone_book[i+1].startsWith(phone_book[i])) return false;
-            
+        HashSet<String> set = new HashSet<>();
+        
+        for(String num : phone_book){
+            set.add(num);
         }
         
-        return answer;
+        for(int i =0; i< set.size(); i++){
+            for(int j=0; j<phone_book[i].length(); j++){
+                if(set.contains(phone_book[i].substring(0,j))) return false;
+            }
+        }
+        
+        return true;
+        
     }
 }
