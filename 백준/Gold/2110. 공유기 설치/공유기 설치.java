924109c9b9c3708ac4c9a1ce4a838d1cc3fbf395
Shin-seung-hyun <jjack1028@naver.com>
2023-02-02 21:31:10 +0900

    add: 공유기 설치 주석 추가

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -23,19 +23,22 @@ public class Main{
         Arrays.sort(arr , 1 ,N+1);
 
         //이분 탐색
+            //공유기를 C개 설치할 때, 공유기 사이 거리의 최대값은?
+            //공유기 사이의 거리가 x일 때, 공유기 C개를 설치할 수 있는가?
         search();
     }
 
     public static boolean determination(int D){
-
+        
+        //공유기는 첫번째 좌표에 무조건 설치한다고 가정
         int cnt =1;
-        int last = arr[1];
+        int install = arr[1];
 
-        for(int i =2; i<=N; i++){
-            if(arr[i] - last < D) continue;
-
-            last = arr[i];
-            cnt++;
+        for(int i =2 ;i <= N; i++){
+            if((arr[i] - install) >= distance){
+                install = arr[i];
+                cnt++;
+            }
         }
 
         return cnt >= C;
@@ -47,6 +50,8 @@ public class Main{
         int right = 1_000_000_000; //10억
         int result = 0;
 
+        //가능한 설치 거리 중, 가장 작은 설치 거리
+            //현재 거리가 설치 가능하다면 더 먼거리가 설치 가능한지 확인
         while(left <= right){
             int mid = (left + right) /2;
 
