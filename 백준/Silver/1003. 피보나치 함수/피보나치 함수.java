01c26a3c51906d275600d5a94fe2baf4587922d4
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-03-08 20:10:23 +0900

    [Silver III] Title: 피보나치 함수, Time: 212 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+//동적 프로그래밍(DP)
+public class Main{
+
+    static StringBuilder sb = new StringBuilder();
+    static int N;
+    static int[][] Dy;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int testCase = sc.nextInt();
+
+        for(int i =1; i<=testCase; i++){
+            N = sc.nextInt();
+
+            //Dy[N][0] : N을 호출했을 때, 0이 몇 번 나오는지
+            //Dy[N][1] : N을 호출했을 때, 1이 몇 번 나오는지
+            Dy = new int[N+1][2];
+
+            DP(N);
+
+            sb.append(Dy[N][0]).append(" ").append(Dy[N][1]).append("\n");
+
+        }
+
+        System.out.print(sb.toString());
+    }
+
+    static void DP(int x){
+
+        //초기화
+        Dy[0][0] = 1;
+        Dy[0][1] = 0;
+
+
+
+        if(x>=1){
+            Dy[1][0] = 0;
+            Dy[1][1] = 1;
+            
+            if(x>=2){
+
+                for(int i =2; i<=N; i++){
+
+                    Dy[i][0] = Dy[i-1][0] + Dy[i-2][0];
+                    Dy[i][1] = Dy[i-1][1] + Dy[i-2][1];
+                }   
+            }
+        }
+
+    }
+}
