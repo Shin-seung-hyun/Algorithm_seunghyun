8c6232e4c515a7170be9e36b30b980eab1db37ca
Shin-seung-hyun <jjack1028@naver.com>
2023-03-24 15:47:17 +0900

    add: 1,2,3 더하기_5 주석 추가

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
@@ -4,6 +4,7 @@ import java.util.*;
 class Main{
     static int N;
     static int [][] Dy;
+    static long [][] dy;
     static int mod = 1_000_000_009;
     static StringBuilder sb = new StringBuilder();
 
@@ -12,6 +13,7 @@ class Main{
         int testCase = sc.nextInt();
 
         Dy = new int[100_000 +1][3+1];
+        dy = new long[100_000 +1][3+1];
 
         DP();
 
@@ -19,11 +21,15 @@ class Main{
 
             N = sc.nextInt();
 
-            int answer = 0;
-            for(int j=1; j<=3; j++){
-                answer = (answer + Dy[N][j]) % mod;
-            }
-            sb.append(answer).append("\n");
+            //출력1
+            sb.append((dy[N][1] + dy[N][2] + dy[N][3]) % mod + "\n");
+
+            //출력2
+//            int answer = 0;
+//            for(int j=1; j<=3; j++){
+//                answer = (answer + Dy[N][j]) % mod;
+//            }
+//            sb.append(answer).append("\n");
         }
 
         System.out.print(sb.toString());
@@ -32,20 +38,27 @@ class Main{
     static void DP(){
 
         //초기값
-        Dy[1][1] = 1;
-        Dy[2][2] = 1;
-        Dy[3][1] = Dy[3][2] = Dy[3][3] = 1;
+        dy[1][1] = 1;
+        dy[2][2] = 1;
+        dy[3][1] = dy[3][2] = dy[3][3] = 1;
 
-        //점화식
+        //점화식1
         for(int i =4; i<=100_000; i++){
-            for(int j = 1; j<=3; j++){
-                for(int k =1; k<=3; k++){
-
-                    if(k == j) continue;
-                    Dy[i][j] = (Dy[i][j] +  Dy[i-j][k]) % mod;
-                }
-            }
+            dy[i][1] = (dy[i-1][2] + dy[i-1][3]) % mod;
+            dy[i][2] = (dy[i-2][1] + dy[i-2][3]) % mod;
+            dy[i][3] = (dy[i-3][1] + dy[i-3][2]) % mod;
         }
+
+        //점화식1
+//        for(int i =4; i<=100_000; i++){
+//            for(int j = 1; j<=3; j++){
+//                for(int k =1; k<=3; k++){
+//
+//                    if(k == j) continue;
+//                    Dy[i][j] = (Dy[i][j] +  Dy[i-j][k]) % mod;
+//                }
+//            }
+//        }
 
     }
 }
