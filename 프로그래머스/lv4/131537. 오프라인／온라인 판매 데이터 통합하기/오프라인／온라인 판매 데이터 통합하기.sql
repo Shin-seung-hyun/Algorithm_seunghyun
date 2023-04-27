37251ca81267bc3c87d831de435c33c25475088a
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-04-27 22:11:22 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -0,0 +1,16 @@
+(select  date_format(SALES_DATE, '%Y-%m-%d') as SALES_DATE , 
+        PRODUCT_ID,
+        USER_ID,
+        SALES_AMOUNT
+from ONLINE_SALE
+where date_format(SALES_DATE, '%Y-%m') like '2022-03')
+
+union
+
+(select  date_format(SALES_DATE, '%Y-%m-%d') as SALES_DATE , 
+        PRODUCT_ID, 
+        null as USER_ID,
+        SALES_AMOUNT
+from OFFLINE_SALE
+where date_format(SALES_DATE, '%Y-%m') like '2022-03')
+order by SALES_DATE asc, PRODUCT_ID asc, USER_ID asc;
