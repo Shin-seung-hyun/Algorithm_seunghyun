2d22591d48d29e43d5c10be86c3d8c4bc2a73d01
Shin-seung-hyun <jjack1028@naver.com>
2023-05-02 20:05:10 +0900

    refactor: 용돈 관리 코드 리팩토링

--- 백준/Silver/6236. 용돈 관리/용돈 관리.java
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -1,30 +1,48 @@
 import java.util.*;
 
+//이분 탐색
 //매개 변수 탐색 알고리즘
-    // 최소 금액 k을 출력하시오
-    // 최소 금액 k로 M번만 인출할 수 있는가?
-class Main{
-    static int N;   // N 일 동안
-    static int M;   // M 번 인출
-    static int [] arr;  //사용한 금액
+// M번만 인출할 수 있을 때, 최소 필요한 금액 K는?
+// 금액이 K일 때 ,M번만 인출할 수 있는가?
+public class Main{
+
+    public static int N;        //일
+    public static int M;        //인출 횟수
+    public static int arr[];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         N = sc.nextInt();
         M = sc.nextInt();
 
-        arr = new int[N+1];
-
-        for(int i =1; i<=N; i++){
+        arr = new int [N+1];
+        for(int i =1; i<=N ;i++){
             arr[i] = sc.nextInt();
         }
 
         search();
     }
 
-    public static  void search(){
-        
+    public static boolean determination(int money){
+        int cnt = 1;
+        int tmp = money;
+
+        for(int i =1; i<=N ; i++){
+            tmp = tmp - arr[i];
+
+            if(tmp < 0){
+                cnt++;
+                tmp = money;
+                tmp = tmp - arr[i];
+            }
+        }
+
+        return cnt <= M;
+    }
+
+    public static void search(){
+
+        //최소 금액 k의 범위 : 쓸 금액의 최대 <= K <= 100_000(번) * 10_000(원)
         int left = arr[1];
         int right = 1_000_000_000;
         int result = 0;
@@ -32,35 +50,20 @@ class Main{
         for(int i =1; i<=N; i++){
             left = Math.max(left, arr[i]);
         }
+
         while(left <= right){
 
-            int mid = (left + right) /2;
+            int mid = (left + right)/2;
 
-            int extra_money = mid;
-            int cnt = 1;
-
-            for(int i =1; i<=N; i++) {
-
-                if(extra_money - arr[i] < 0){
-                    //extra_money += mid;
-                    extra_money = mid;
-                    cnt++;
-                }
-                extra_money -= arr[i];
-
-            }
-            if(cnt > M){
-                left = mid + 1;
-            }
-            else{
-                right = mid - 1;
+            if(determination(mid)){
+                right = mid -1;
                 result = mid;
             }
-
+            else{
+                left = mid +1;
+            }
         }
-
         System.out.println(result);
     }
-
 
 }
