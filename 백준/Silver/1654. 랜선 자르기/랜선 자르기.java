339f135f48f01ea56caf5c17d581f236eaa24c4a
Shin-seung-hyun <jjack1028@naver.com>
2023-05-09 16:22:33 +0900

    refactor: 랜선 자르기 코드 리팩토링

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -7,50 +7,55 @@ import java.io.*;
     //길이 C로 잘랐을 때, N개의 랜선을 얻을 수 있는가?
 public class Main{
 
-    public static int N,K;
-    public static int arr[];
+    public static int K;    // 이미 가지고 있는 랜선의 수
+    public static int N;    // 필요한 랜선의 수
+    public static int []arr;
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
-        N = sc.nextInt();
         K = sc.nextInt();
+        N = sc.nextInt();
 
-        arr = new int[N + 1];
-
-        for(int i = 1; i<=N; i++){
+        arr = new int[K + 1];
+        for (int i = 1; i <= K; i++) {
             arr[i] = sc.nextInt();
         }
-        
-        System.out.println(search());
+
+        search();
     }
 
     public static boolean Possible(long H){
-        long sum = 0;
-        for(int i  =1; i<=N ; i++){
-            sum += arr[i] / H;
+        int cnt = 0;
+
+        for(int i =1; i<=K; i++){
+            cnt += (arr[i] / H);
         }
 
-        return sum >=K;
+        return N <= cnt;
     }
-    public static long search(){
+
+    public static long search() {
         long left = 1;
-        long right = Integer.MAX_VALUE;
-        long result =0;
+        long right = Integer.MIN_VALUE;
+        long result = 0;
 
-        while(left <= right){
-            long mid = (left + right)/2;
+        for (int i = 1; i <= K; i++) {
+            right = Math.max(right, arr[i]);
+        }
 
-            if(Possible(mid)){
-                left = mid +1;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            if (Possible(mid)) {
+                left = mid + 1;
                 result = mid;
             }
-            else{
-                right = mid-1;
-            }
+            else right = mid - 1;
+
         }
 
-        return result;
+        System.out.println(result);
     }
 
 }
