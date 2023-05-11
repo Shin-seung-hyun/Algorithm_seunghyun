cdce64dd1330d86daddafac00ce6a20301400f30
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-05-11 21:56:27 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1752 ms, Memory: 329464 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,58 +1,59 @@
 import java.util.*;
 
-//그래프와 DFS, BFS
-    //인접 행렬
-
-//연결 요소 찾기
-    // 끊겨 있는 연결 요소 부분은 DFS나 BFS 탐색이 끝난 후에도 탐색이 안 될 것이다.
-    // 반면, 같은 연결 요소에 속한 정점들은 한번의 DFS나 BFS로 이미 해당 정점을 방문할 것이다.
 class Main{
-
-    static int N;   //정점의 개수
-    static int M;   //간선의 개수
-    static boolean visit[];   //방문 여부 확인
-    static ArrayList<Integer> [] arrList;   //인접 행렬
-    static int answer = 0;
+    static int N;   // 정점의 수
+    static int M;   // 간선의 수
+    static ArrayList<Integer> [] adj;
+    static boolean []visit;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         N = sc.nextInt();
         M = sc.nextInt();
 
+        adj = new ArrayList[N+1];
         visit = new boolean[N+1];
 
-        arrList = new ArrayList[N+1];
-        for(int i =1; i<= N; i++){
-            arrList[i] = new ArrayList<>();
-        }
-        for(int i =1; i<= M; i++){
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-
-            arrList[u].add(v);
-            arrList[v].add(u);
-        }
-
         for(int i =1; i<=N; i++){
-            if(visit[i]) continue;
-            DFS(i);
-            answer++;
+            adj[i] = new ArrayList<>();
         }
 
-        System.out.println(answer);
+        for(int i =1; i<=M; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            adj[x].add(y);
+            adj[y].add(x);
+        }
+
+        int cnt = 0;
+        for(int i =1; i<=N; i++){
+
+            if(visit[i]) continue;
+            BFS(i);
+            cnt++;
+        }
+
+        System.out.println(cnt);
     }
 
-    static void DFS(int x ){
+    public static void BFS(int start){
+        Queue<Integer> queue = new LinkedList<>();
 
-        //visit 체크
-        visit[x] = true;
+        queue.add(start);
+        visit[start] = true;
 
-        //반복
-        for(int y : arrList[x]){
+        while(!queue.isEmpty()){
+            int x = queue.poll();
 
-            if(visit[y]) continue;
-            DFS(y);
+            for(int val : adj[x]){
+
+                if(visit[val]) continue;
+
+                visit[val] = true;
+                queue.add(val);
+            }
         }
-
     }
 }
