62db96b5e7f959d9dd733a87185afffaced58a95
Shin-seung-hyun <jjack1028@naver.com>
2023-05-12 15:49:21 +0900

    add: 1로 만들기 주석 추가

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+//DP
 class Main{
     static int X;
     static int[][] Dy;
@@ -23,18 +24,22 @@ class Main{
         for(int i =2; i<=X; i++){
 
             // 6으로 나눠질 경우
+            // 3으로 나누는 경우, 2로 나누는 경우, 1을 빼는 경우 비교
             if(i % 6 ==0){
                 Dy[0][i] = Math.min(Math.min(Dy[0][i/3],Dy[1][i/3]), Math.min(Dy[0][i/2],Dy[1][i/2])) + 1;
             }
             // 3으로 나눠질 경우
+            //3으로 나누는 경우, 1을 빼는 경우 비교
             else if(i %3 == 0){
                 Dy[0][i] = Math.min(Dy[0][i/3],Dy[1][i/3]) + 1;
             }
             // 2로 나눠질 경우
+            // 2로 나누는 경우, 1을 빼는 경우 비교
             else if(i %2 == 0){
                 Dy[0][i] = Math.min(Dy[0][i/2],Dy[1][i/2]) + 1;
             }
             // 모두 아닐 경우
+            //1을 빼는 경우 비교
             else{
                 Dy[0][i] = Math.min(Dy[0][i-1],Dy[1][i-1]) +1;
             }
