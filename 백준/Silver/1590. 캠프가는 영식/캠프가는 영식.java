c029f482045b14f9301a7586a428e0cfe06ed5f8
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-05-16 21:13:44 +0900

    [Silver IV] Title: 캠프가는 영식, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/1590. 캠프가는 영식/캠프가는 영식.java
+++ 백준/Silver/1590. 캠프가는 영식/캠프가는 영식.java
@@ -1,63 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N, T;
-	static ArrayList<Long> times;
-	
-	static long bs()
-	{
-		if (times.get(times.size() - 1) < T) return -1;
-		if (times.get(0) >= T) return times.get(0) - T;
-		
-	    long l = 0;
-	    long r = times.size() - 1;
-	    long m = (l + r) / 2;
-	    while (l < r)
-	    {
-	        m = (l + r) / 2;
-	        if (times.get((int)m) == T)
-	        {
-	            return 0;
-	        }
-	        else if (times.get((int)m) > T) {
-	        	r = m;
-	        }
-	        else
-	        {
-	            l = m + 1;
-	        }
-	    }
-	    return times.get((int)r) - T;
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		T = Integer.parseInt(st.nextToken());
-		
-		times = new ArrayList<>();
-		
-		for (int i = 0; i < N ; i++) {
-			st = new StringTokenizer(br.readLine());
-			long start = Long.parseLong(st.nextToken());
-			long gap = Long.parseLong(st.nextToken());
-			long num = Long.parseLong(st.nextToken());
-			
-			for (int j = 0; j < num; j++) {
-				times.add(gap * j + start);
-			}
-		}
-		
-		Collections.sort(times);
-		long answer = bs();
-		
-		System.out.println(answer);
-	}
+
+    static int N;   // 버스 종류
+    static int T;   // 터미널 도착 시간
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer> busTime = new ArrayList<>();
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int S = Integer.parseInt(st.nextToken());
+            int I = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+
+            // 버스의 모든 시간 입력
+            for (int j = 1; j <= C; j++) {
+                busTime.add(S);
+                S = S + I;
+            }
+        }
+
+        // 버스의 모든 시간 정렬
+        Collections.sort(busTime);
+
+        for(int time : busTime) {
+            if (T <= time) {
+                System.out.println(time - T);
+                return;
+            }
+        }
+        System.out.println(-1);
+    }
 }
