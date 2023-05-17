798641d3f607387b1dd885ebb170a30271699588
Shin-seung-hyun <jjack1028@naver.com>
2023-05-17 20:06:40 +0900

    refactor: 부분합 코드 리팩토링

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -25,35 +25,19 @@ public class Main{
         int sum = 0;
         int answer = N+1;   // answer의 최대값(N)을 넘는 범위 설정
 
-        for(int left = 1; left <= N; left++){
+        for(int left =1; left <=N; left++){
 
-            //하나씩 줄이기
-            sum = sum -arr[left-1];
-
-            //S가 될 때까지 더하기
-            while(right + 1 <= N && sum < S){
-                sum = sum + arr[++right];
+            while(right+1 <=N && sum <S){
+                right++;
+                sum += arr[right];
             }
 
             if(sum >= S){
-                //몇개를 더해서 S가 됐는지 확인
-                answer = Math.min(answer, right -left +1);
+                result = Math.min(result, right - left + 1);
+                sum -= arr[left];
             }
+
         }
-
-
-//        for(int left =1; left <=N; left++){
-//
-//            while(sum < S && ++right <=N){
-//                sum += arr[right];
-//            }
-//
-//            if(sum >= S){
-//                result = Math.min(result, right - left + 1);
-//                sum -= arr[left];
-//            }
-//
-//        }
 
         if(answer == N+1) answer =0;
         System.out.println(answer);
