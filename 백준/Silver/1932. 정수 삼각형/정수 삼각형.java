f25f7ffadcc4ab6fc6b1e2c15bfbf5394128b3d0
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-05-19 23:04:18 +0900

    [Silver I] Title: 정수 삼각형, Time: 312 ms, Memory: 27248 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,44 +1,47 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main{
-    static int N;   // 삼각형의  크기
-    static int[][] arr;
-    static int[][] Dy;
+    static int N;
+    static int [][]arr;
+    static int [][]Dy;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        Dy = new int [N+1][N+1];
-        arr = new int [N+1][N+1];
+        arr = new int[N+1][N+1];
+        Dy = new int[N+1][N+1];
 
-        for (int i = 1; i <= N; i++) {
+        for(int i=1; i<=N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j =1; j<=i; j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        //( i , j )가 선택되기 전의 왼쪽 위 ( i-1, j-1), 오른쪽 위 ( i-1, j )
-        // 점화식
-        // Dy[i][j] = Max(Dy[i-1][j] , Dy[i-1][j-1] ) + arr[i][j]
-
-        for(int i =1; i<=N; i++){
-            for(int j =1; j<=N; j++){
-
-                Dy[i][j] = Math.max(Dy[i-1][j-1], Dy[i-1][j]) + arr[i][j];
-            }
-        }
-
+        DP();
 
         // 가장 마지막 줄에서 가장 큰 값 찾기
-        int answer = 0;
+        int answer =0;
         for(int i =1; i<=N; i++){
-            if(answer < Dy[N][i]) answer = Dy[N][i];
+            answer = Math.max(answer, Dy[N][i]);
         }
 
         System.out.println(answer);
     }
 
+    static void DP(){
+
+        // 점화식
+        //( i , j )가 선택되기 전의 왼쪽 위 ( i-1, j-1), 오른쪽 위 ( i-1, j )
+        // Dy[i][j] = Max(Dy[i-1][j] , Dy[i-1][j-1] ) + arr[i][j]
+
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=N; j++){
+
+                Dy[i][j] = Math.max(Dy[i-1][j-1] , Dy[i-1][j]) + arr[i][j];
+            }
+        }
+    }
 }
