78b49264b353441438a2e187ca4ebbadf1242a5b
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-05-24 21:20:25 +0900

    [Gold III] Title: 세 용액, Time: 248 ms, Memory: 17472 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -5,6 +5,7 @@ public class Main {
     static int N;   //용액의 수
     static long[] arr;
     static long[] answer;
+    static long interval;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,31 +23,40 @@ public class Main {
         //-97 -6 -2 6 98
         Arrays.sort(arr,1,N+1);
 
-        long diff = Long.MAX_VALUE;
+        interval = Long.MAX_VALUE;
 
         // 가장 왼쪽부터 시작해서 가장 오른쪽 끝까지
         for(int i = 1; i <= N-2; i++) {
-            int left = i+1;
-            int right = N;
-
-            while(left < right) {
-
-                long sum = arr[i] + arr[left] + arr[right];
-
-                // 차이가 더 작다면 원소 저장
-                if(Math.abs(sum) < diff) {
-                    diff = Math.abs(sum);
-                    answer[0] = arr[i];
-                    answer[1] = arr[left];
-                    answer[2] = arr[right];
-                }
-
-                if(sum > 0) right--;
-                else left++;
-
-            }
+            twoPointer(i);
         }
 
         System.out.println(answer[0] + " " + answer[1] + " " + answer[2]);
     }
+
+
+    static void twoPointer(int i){
+        int left = i+1;
+        int right = N;
+
+        while(left < right) {
+
+            // arr,asnwer 배열이 int 라면 아래와 같이 해야 함.
+            //long sum = Long.valueOf(arr[i] + Long.valueOf(arr[left] + arr[right]));
+            long sum = arr[i] + arr[left] + arr[right];
+
+            if(Math.abs(sum) < interval) {
+
+                interval = Math.abs(sum);
+
+                answer[0] = arr[i];
+                answer[1] = arr[left];
+                answer[2] = arr[right];
+            }
+
+            if(sum > 0) right--;
+            else left++;
+
+        }
+    }
+
 }
