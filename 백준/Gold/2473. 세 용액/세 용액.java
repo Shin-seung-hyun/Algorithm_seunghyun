0b63cc774f9e8693a1b0bd77b785a282cff5c914
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-05-24 21:11:29 +0900

    [Gold III] Title: 세 용액, Time: 252 ms, Memory: 17468 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -1,53 +1,52 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		long[] solutions = new long[N];
-		
-		StringTokenizer stt = new StringTokenizer(br.readLine());
-		
-		for(int i = 0; i < N; i++) {
-			int next = Integer.parseInt(stt.nextToken());
-			solutions[i] = next;
-		}
-		
-		Arrays.sort(solutions);
-		
-		long[] res = new long[3];
-		
-		long diff = Long.MAX_VALUE;
+    static int N;   //용액의 수
+    static long[] arr;
+    static long[] answer;
 
-		// 가장 왼쪽부터 시작해서 가장 오른쪽 끝까지
-		for(int i = 0; i < N; i++) {
-			int left = i+1; // 가장 왼쪽을 제외하고 그 다음부터 순회
-			int right = N-1;
-			
-			while(left < right) {
-				long sum = solutions[i] + solutions[left] + solutions[right];
-				
-				long cur_diff = Math.abs(sum);
-				
-				if(cur_diff < diff) {
-					diff = cur_diff;
-					res[0] = solutions[i];
-					res[1] = solutions[left];
-					res[2] = solutions[right];
-				}
-				
-				if(sum > 0) { // 차이가 0보다 크다. 오른쪽 원소를 한 칸 앞으로
-					right--;
-				} else { // 차이가 0보다 작다. 왼쪽 원소를 한 칸 앞으로
-					left++;
-				}
-			}
-		}
-		System.out.println(res[0] + " " + res[1] + " " + res[2]);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        answer = new long[3];
+        arr = new long[N+1];
+        st = new StringTokenizer(br.readLine());
+        for(int i =1; i<=N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        //-97 -6 -2 6 98
+        Arrays.sort(arr,1,N+1);
+
+        long diff = Long.MAX_VALUE;
+
+        // 가장 왼쪽부터 시작해서 가장 오른쪽 끝까지
+        for(int i = 1; i <= N-2; i++) {
+            int left = i+1;
+            int right = N;
+
+            while(left < right) {
+
+                long sum = arr[i] + arr[left] + arr[right];
+
+                // 차이가 더 작다면 원소 저장
+                if(Math.abs(sum) < diff) {
+                    diff = Math.abs(sum);
+                    answer[0] = arr[i];
+                    answer[1] = arr[left];
+                    answer[2] = arr[right];
+                }
+
+                if(sum > 0) right--;
+                else left++;
+
+            }
+        }
+
+        System.out.println(answer[0] + " " + answer[1] + " " + answer[2]);
+    }
 }
