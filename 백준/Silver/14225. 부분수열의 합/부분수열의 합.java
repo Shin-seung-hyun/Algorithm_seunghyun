90cc311b50711bc1bd57cb554d98cc7637863a1f
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-06-07 15:03:01 +0900

    [Silver I] Title: 부분수열의 합, Time: 148 ms, Memory: 16372 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.io.*;
 
 // 수열의 부분합으로 나올 수 없는 가장 작은 수
-public class Main {
+/// 시간복잡도 O(2^20)
+public class Main{
 
-    static int N;
-    static int[] S;
-    static int[] arr = new int [20+1];
-    static boolean[] visit;
+    static int N;   // 수열의 크기
+    static int[] S; // 수열
+    static boolean[] visit; // 부분합
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,20 +15,16 @@ public class Main {
         N = Integer.parseInt(br.readLine());
 
         visit = new boolean[2_000_000 + 1];
-        S = new int [N+1];
+        S = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i =1; i<=N; i++){
+        for (int i = 1; i <= N; i++) {
             S[i] = Integer.parseInt(st.nextToken());
-            visit[S[i]] = true;
         }
+        
+        // 부분 수열의 합으로 만들 수 있는 것 완전탐색
+        DFS(1,0);
 
-        Arrays.sort(S,1,N+1);
-
-        for(int i=2; i<=N; i++){
-            DFS(1,1, i);
-            Arrays.fill(arr,0);
-        }
-
+        // 부분 수열의 합으로 만들 수 없는 가장 작은 자연수 출력
         for(int i=1; i<=2_000_000; i++){
             if(visit[i]) continue;
             else{
@@ -36,28 +32,24 @@ public class Main {
                 return;
             }
         }
+
     }
 
-    static void DFS(int at, int depth, int M){
+    static void DFS(int depth, int sum){
 
-        //종료 조건
-        if( depth == M+1){
-            int sum =0;
-            for(int i = 1; i<=20; i++){
-                sum += arr[i];
-            }
-
+    // 종료 조건
+        if(depth == N+1){
             visit[sum] = true;
             return;
         }
 
-        // 반복 조건
-        for(int i = at; i<=N; i++){
-            arr[depth] = S[i];
-            DFS(i+1,depth+1, M);
-        }
+    // 반복 조건
+        //S[]를 안 사용하는 경우
+        DFS(depth+1, sum);
+
+        // S[]를 사용하는 경우
+        DFS(depth+1, sum + S[depth]);
 
     }
-
 
 }
