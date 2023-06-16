4da566dd227f5512e1d7544535c9436c7114253c
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-06-16 21:47:01 +0900

    [Silver I] Title: 추월, Time: 132 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/2002. 추월/추월.java
+++ 백준/Silver/2002. 추월/추월.java
@@ -1,13 +1,10 @@
 import java.util.*;
 import java.io.*;
 
-//문자열
-public class Main {
+class Main{
 
-    static int N;   // 차량수
-    static String[] in;
-    static String[] out;
-    static boolean[] visit;
+    static int N;
+    static int[] arr;
     static int answer = 0;
 
     public static void main(String[] args) throws IOException {
@@ -15,41 +12,28 @@ public class Main {
 
         N = Integer.parseInt(br.readLine());
 
-        in = new String[N+1];
-        out = new String[N+1];
-        visit = new boolean[N+1];
+        HashMap<String, Integer> hashMap = new HashMap<>();
 
-        // 터널의 입구
-        for(int i =1; i<=N; i++){
-            in[i] = br.readLine();
+        for (int i = 1; i <= N; i++) {
+            hashMap.put(br.readLine(), i);
         }
 
-        // 터널의 출구
-        for(int i =1; i<=N; i++){
-            out[i] = br.readLine();
+        arr = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            // arr[]에 탈출 순서대로 입장번호 저장
+            arr[i] = hashMap.get(br.readLine());
         }
 
+        for (int i = 1; i < N; i++) {
+            for (int j = i + 1; j <= N; j++) {
 
-        // 확인
-        for(int i =1; i<=N; i++){
-            for(int j =1; j<=N; j++){
-
-                if(in[i].equals(out[j])){
-                    visit[j] = true;
+                if (arr[i] > arr[j]) {
+                    answer++;
                     break;
-                }
-
-                // 나를 만나기 전에 나오는 차들은 모두 자신을 추월한 차량임
-                else{
-                    if(!visit[j]){
-                        answer++;
-                        visit[j] = true;
-                    }
                 }
             }
         }
 
         System.out.println(answer);
     }
-
 }
