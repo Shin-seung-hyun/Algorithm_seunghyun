5634e6ab8da6c5b55d0f0c4cb7ad3e26c314fd23
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-06-19 20:59:45 +0900

    [Silver V] Title: 줄세우기, Time: 224 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+//
+class Main {
+
+    static int[] height;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int P = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= P; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int classNum = Integer.parseInt(st.nextToken());
+
+            height = new int[20 + 1];
+            for (int j = 1; j <= 20; j++) {
+                height[j] = Integer.parseInt(st.nextToken());
+            }
+
+            //순서 정렬
+            int cnt = 0;
+            for(int k = 1; k<=20; k++){
+                // height[k] 보다 index가 작은 배열 중에 큰 값이 있나 확인
+                for(int l = k-1; l>=1; l--){
+
+                    if(height[k] < height[l]) cnt++;
+                }
+            }
+
+            sb.append(classNum +" " + cnt + "\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
