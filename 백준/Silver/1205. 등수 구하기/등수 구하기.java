306ea608fbdb245e75ed37d321bf9f5a8013eb00
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-06-22 23:27:10 +0900

    [Silver IV] Title: 등수 구하기, Time: 128 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.java
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -26,23 +26,22 @@ class Main{
         for(int i=1; i<=N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        arr[N+1] = newScore;
 
         //Collections.reverseOrder()를 적용하기 위해서는
         // sort 메서드의 첫번째 인자가 T[]임으로 Integer[] 로 변경이 필요함
         // int[] -> Integer[]
         Integer[] conv = Arrays.stream(arr).boxed().toArray(Integer[]::new);
 
-        Arrays.sort(conv,1,N+2, Collections.reverseOrder() );
+        Arrays.sort(conv,1,N+1, Collections.reverseOrder() );
 
         // 랭킹 리스트가 다 차있고, 제일 낮은 점수보다 새로운 점수가 낮거나 같을 때 -1
-        if( N == P && conv[N+1] >= newScore){
+        if( N == P && conv[N] >= newScore){
             System.out.println(-1);
             return;
         }
 
         int rank = 1;
-        for(int i=1; i<=N+1; i++){
+        for(int i=1; i<=N; i++){
 
             if(conv[i] > newScore) rank++;
         }
