fc432a6d4a18a0738d7900b48f60c8c059350504
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-07-13 15:16:46 +0900

    [Silver III] Title: 블로그, Time: 376 ms, Memory: 35092 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -1,12 +1,12 @@
 import java.util.*;
 import java.io.*;
 
-// 슬라이딩 윈도우
+// 투 포인터
 class Main{
 
     static int N;   //전체 일수
     static int X;   //알고 싶은 기간
-    static int[] arr;
+    static int[] arr;   // 방문객 배열
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,35 +21,39 @@ class Main{
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        solve();
+        twoPointer();
     }
 
-    static void solve(){
+    static void twoPointer(){
+        int right = 1;
         int sum = 0;
-        for(int i=1; i<=X; i++) sum += arr[i];
+        int max = 0;
+        int maxCnt = 1;
 
-        int max = sum;
-        int maxCnt =1;
-        for(int i= X+1; i<=N; i++){
+        for(int left =1; left<= N - X + 1; left++){
 
-            // arr[i] : 새로운 것은 더하고
-            // arr[i-X]: 기존의 것은 빼자
-            sum = sum + arr[i] - arr[i-X];
+            while(right <= N && right - left +1 <= X){
+                sum += arr[right];
+                right++;
+            }
 
-            if(max == sum) maxCnt++;
-
-            // 최댓값 갱신
-            else if ( max < sum){
+            if( max == sum) maxCnt++;
+            else if(sum > max){
                 max = sum;
                 maxCnt = 1;
             }
+
+            sum -= arr[left];
         }
 
-        if( max == 0) System.out.println("SAD");
-        else {
-            System.out.println(max);
-            System.out.println(maxCnt);
+
+        // 출력
+        if( max == 0){
+            System.out.println("SAD");
+            return;
         }
+
+        System.out.println(max);
+        System.out.println(maxCnt);
     }
-
 }
