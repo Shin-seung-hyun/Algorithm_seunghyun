fb65cf009ab232a404f374e3cdefb5fb2eb04fcb
Shin-seung-hyun <jjack1028@naver.com>
2023-07-17 21:47:16 +0900

    add : 소트인사이드 풀이 추가

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
@@ -22,5 +24,19 @@ public class Main {
         for(int i =0; i< arr.length; i++){
             System.out.print(arr[i]);
         }
+
+    /* 방법2
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        char [] charArray = str.toCharArray();
+
+        Arrays.sort(charArray);
+
+        for(int i = charArray.length-1; i >0; i--){
+            System.out.print(charArray[i]);
+        }
+
+     */
     }
 }
