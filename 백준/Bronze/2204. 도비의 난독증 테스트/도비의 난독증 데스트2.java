562cef6b5a1b529fa4c23e5a3c427ad723d65843
Shin-seung-hyun <jjack1028@naver.com>
2023-07-18 15:14:30 +0900

    add: 도비의 난독증 풀이 추가

--- 백준/Bronze/2204. 도비의 난독증 테스트/도비의 난독증 데스트2.java
+++ 백준/Bronze/2204. 도비의 난독증 테스트/도비의 난독증 데스트2.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 //문자열
 // 대소문자를 구분하지 않고 사전순으로 가장 앞서는 문자열 출력
-class test{
+class Main{
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,13 +14,15 @@ class test{
 
             if(N == 0) break;
 
-            List<String> list = new ArrayList<>();
+            ArrayList<String> arrList = new ArrayList<>();
 
             for(int i =0; i<N; i++){
-                list.add(br.readLine());
+                arrList.add(br.readLine());
             }
 
-            Collections.sort(list, new Comparator<String>() {
+        /* ArrayList 정렬 */
+            // 방법1. 익명 클래스로 Comparator 생성
+            Collections.sort(arrList, new Comparator<String>() {
 
                 @Override
                 public int compare(String o1, String o2) {
@@ -28,7 +30,10 @@ class test{
                 }
             });
 
-            System.out.println(list.get(0));
+            // 방법2. 람다식으로 Comparator 생성
+            Collections.sort(, (o1,   o2) -> o1.toUpperCase().compareTo(o2.toUpperCase() ));
+
+            System.out.println(arrList.get(0));
         }
 
     }
