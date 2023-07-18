2713d2f786fc9aa443b5d8e54191b6ebb84c07b9
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-07-18 15:56:23 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 120 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -8,45 +8,38 @@ class Main{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
-
+    
+        int cnt =0;
         int len = str.length();
-        int count = 0;
-
-        for (int i = 0; i < len; i++) {
-
+        
+        for(int i= 0; i< str.length(); i++){
+            
             char ch = str.charAt(i);
-
-            // c=, c-
-            if(ch == 'c' && i < len - 1) {	
-                if(str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-') i++;
+            
+            // c= , c-
+            if( ch =='c' && i+1 <len){
+                if( str.charAt(i+1) == '=' || str.charAt(i+1) == '-') i++;
             }
-
-            //d-, dz=
-            else if(ch == 'd' && i < len - 1) {
-                
-                //d-
-                if(str.charAt(i + 1) == '-') i++;
-                
-                //dz=
-                else if(str.charAt(i + 1) == 'z' && i < len - 2) {
-
-                    if(str.charAt(i + 2) == '=') i += 2;
-                }
+            
+            //dz=, d-
+            else if ( ch == 'd' && i+1 <len){
+                if ( str.charAt(i+1)== '-') i++;
+                else if (i+2 <len && str.charAt(i+1) == 'z' && str.charAt(i+2) == '=' ) i+=2;
             }
-
-            // lj, nj
-            else if((ch == 'l' || ch == 'n') && i < len - 1) {
-                if(str.charAt(i + 1) == 'j') i++;
+            
+            //lj, nj
+            else if ( (ch == 'l' || ch == 'n') && i+1 < len){
+                if( str.charAt(i+1) == 'j') i++;
             }
-
-            // s=, z= 
-            else if((ch == 's' || ch == 'z') && i < len - 1) {
-                if(str.charAt(i + 1) == '=') i++;
+            
+            //s=, z=
+            else if ( (ch =='s' || ch =='z') && i +1 < len){
+                if( str.charAt(i+1) == '=') i++;
             }
-
-            count++;
+            
+            cnt++;
         }
 
-        System.out.println(count);
+        System.out.println(cnt);
     }
 }
