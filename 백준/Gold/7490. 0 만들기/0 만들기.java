92c554d170e425da9e8b022a18742e2357337787
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-08-16 16:01:01 +0900

    [Gold V] Title: 0 만들기, Time: 192 ms, Memory: 17088 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -1,70 +1,42 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-// DFS
 public class Main {
-
-    static char[] op = new char[]{ '+', '-', ' '};
-    static StringBuilder sb = new StringBuilder();
-    static ArrayList<String > arrList;
     static int N;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //StringTokenizer st  = new StringTokenizer(br.readLine());
 
         int testCase = Integer.parseInt(br.readLine());
         for(int t =1; t<= testCase; t++){
 
-
             N = Integer.parseInt(br.readLine());
 
-            arrList = new ArrayList<>();
+            sb = new StringBuilder();
 
-            DFS(1, "1");
+            DFS(1, 1, 0, 1, "1");
 
-            // 정렬
-            Collections.sort(arrList);
-
-            // 출력
-            for( String s  : arrList) System.out.println(s);
-            System.out.println();
+            System.out.println(sb.toString());
         }
-
     }
-
-    static void DFS(int depth, String str){
-        // 중복 O, 순서 X
+    static void DFS(int depth, int num, int sum, int op, String str) {
 
         // 종료 조건
-        if( depth == N){
-            //System.out.println(str);
+        if(depth == N) {
+            sum += (num * op);
 
-            String s = str.replaceAll(" ", "");
-            
-            //StringTokenizer(String s, 구분자, 구분자 포함여부 T/F)
-                //T :   1, +, 2, +, 3
-                //F :   1, 2, 3
-            StringTokenizer st = new StringTokenizer(s, "+-",true);
-
-            int sum = Integer.parseInt(st.nextToken()); // 1
-
-            while(st.hasMoreTokens()){
-                String op = st.nextToken(); // +
-
-                if( op.equals("+")) sum += Integer.parseInt(st.nextToken());
-                else sum -= Integer.parseInt(st.nextToken());
-            }
-
-            if( sum ==0) arrList.add(str);
+            if(sum == 0) sb.append(str+"\n");
 
             return;
         }
 
         // 반복 조건
-        for(int i = 0; i<3; i++){
-            DFS(depth+1, str +op[i]+ (depth+1) );
-        }
-    }
+        DFS(depth + 1, num * 10 + depth + 1, sum, op, str + " " + (depth + 1));
+        DFS(depth + 1, depth + 1, sum + (num * op), 1,str+"+"+  (depth + 1));
+        DFS(depth + 1, depth + 1, sum + (num * op), -1, str+"-"+ (depth + 1));
 
+    }
 }
