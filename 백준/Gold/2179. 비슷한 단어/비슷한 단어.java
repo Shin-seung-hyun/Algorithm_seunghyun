6d666b0f1ce592b497cdd90734fdb666db8667d2
Shin-seung-hyun <jjack1028@naver.com>
2023-08-18 21:42:32 +0900

    refactor: 비슷한 단어 코드 리팩토링

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -1,43 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
+	static int N;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		ArrayList<String> list = new ArrayList<>();
-		for(int i=0;i<n;i++) {
+		StringTokenizer st;
+
+		N = Integer.parseInt(br.readLine());    // 영단어 수
+
+		ArrayList<String> arrList = new ArrayList<>();
+		for(int i=0;i<N;i++) {
 			String s= br.readLine();
-			if(!list.contains(s))
-				list.add(s);
+
+			// 중복 저장 방지
+			if(!arrList.contains(s))
+				arrList.add(s);
 		}
-		
-		int resultStr1=0;
-		int resultStr2=0;
-		int maxCount=0;
-		for(int i=0;i<n;i++) {
-			String str1 = list.get(i);
-			for(int j=i+1;j<n;j++) {
-				int count=0;
-				String str2=list.get(j);
-				int size=Math.min(str1.length(),str2.length());
-				for(int z=0;z<size;z++) {
-					if(str1.charAt(z)==str2.charAt(z)) count++;
+
+		int result1=0;
+		int result2=0;
+		int maxCnt=0;
+
+		for(int i=0; i<N; i++) {
+			String str1 = arrList.get(i);
+
+			for(int j=i+1; j<N; j++) {
+				int cnt=0;
+				String str2 = arrList.get(j);
+				int size = Math.min(str1.length(),str2.length());
+
+				for(int k =0; k<size; k++) {
+					if(str1.charAt(k) == str2.charAt(k)) cnt++;
 					else break;
 				}
-				if(count>maxCount) {
-					resultStr1=i;
-					resultStr2=j;
-					maxCount=count;
+
+				if(cnt > maxCnt) {
+					result1 = i;
+					result2 = j;
+					maxCnt = cnt;
 				}
 			}
 		}
-		
-		System.out.println(list.get(resultStr1));
-		System.out.println(list.get(resultStr2));
+
+		System.out.println(arrList.get(result1));
+		System.out.println(arrList.get(result2));
 	}
+
 
 }
