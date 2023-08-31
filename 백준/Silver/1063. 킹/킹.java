14559eb21c70916a892f6f9fab0960627ca8a71b
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-09-01 00:02:19 +0900

    [Silver III] Title: 킹, Time: 160 ms, Memory: 16256 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.java
+++ 백준/Silver/1063. 킹/킹.java
@@ -1,69 +1,70 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+//구현
+public class Main{
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         String king = st.nextToken();
-        int kingX = king.charAt(0)-'A'+1;
-        int kingY = king.charAt(1)-'0';
-        
+        int kx = king.charAt(0) - 'A' +1;
+        int ky = king.charAt(1) - '0';
+
         String stone = st.nextToken();
-        int stoneX = stone.charAt(0)-'A'+1;
-        int stoneY = stone.charAt(1)-'0';
-        
-        //          R  L B T RT LT RB LB
-        int[] dx = {1,-1,0,0,1, -1,1, -1};
-        int[] dy = {0,0,-1,1,1, 1, -1,-1};
+        int sx = stone.charAt(0)-'A'+1;
+        int sy = stone.charAt(1) - '0';
 
-        int n = Integer.parseInt(st.nextToken());
-        for(int i=0; i<n; i++) {
-            
-            String move = br.readLine();
-            int idx = 0;
-            int tmpX = kingX, tmpY = kingY;
-            
-            switch (move) {
-                case "R": idx = 0; break;
-                case "L": idx = 1; break;
-                case "B": idx = 2; break;
-                case "T": idx = 3; break;
-                case "RT": idx = 4; break;
-                case "LT": idx = 5; break;
-                case "RB": idx = 6; break;
-                case "LB": idx = 7; break;
-            }
-            
-            tmpX += dx[idx];
-            tmpY += dy[idx];
-            
-            //범위 확인
-            if(tmpX>=1 && tmpX<=8 && tmpY>=1 && tmpY<=8) {
-                if(tmpX==stoneX && tmpY==stoneY) {
-                    
-                    if(stoneX+dx[idx]>=1 && stoneX+dx[idx]<=8 && stoneY+dy[idx]>=1 && stoneY+dy[idx]<=8) {
-                        stoneX += dx[idx];
-                        stoneY += dy[idx];
-                    } 
-                    else continue;
+        int num = Integer.parseInt(st.nextToken());
+
+        for(int i=1; i<=num; i++){
+
+            String str = br.readLine();
+
+            int arr[] = move(str, kx, ky);
+
+            int x = arr[0];
+            int y = arr[1];
+
+            if( x < 1  || y < 1 || x > 8 || y >8 ) continue;
+            if( x == sx && y == sy){
+
+                int arr2[] = move(str,sx,sy);
+                int tx = arr2[0];
+                int ty = arr2[1];
+
+                if(tx < 1 || ty < 1 || tx > 8 || ty >8 ) continue;
+                else{
+                    sx = tx;
+                    sy = ty;
                 }
-                
-                kingX = tmpX;
-                kingY = tmpY;
             }
+
+            kx = x;
+            ky = y;
         }
-        
-        king = "";
-        stone = "";
 
-        king += String.valueOf( (char)(kingX+'A'-1) ) + kingY;
-        stone += String.valueOf( (char)(stoneX+'A'-1) ) + stoneY;
 
-        System.out.println(king);
-        System.out.println(stone);
+        System.out.println( String.valueOf( (char)(kx + 'A' -1)) +  ky );
+        System.out.println( String.valueOf( (char)(sx + 'A' -1)) +  sy );
+    }
+
+    static int[] move(String op, int x, int y){
+
+        switch(op){
+
+            case "R" : x +=1; break;
+            case "L" : x -=1; break;
+            case "B" : y -=1; break;
+            case "T" : y +=1; break;
+            case "RT": x +=1; y +=1; break;
+            case "LT": x -=1; y +=1; break;
+            case "RB": x +=1; y -=1; break;
+            case "LB": x -=1; y -=1; break;
+        }
+
+        return new int[]{x, y};
     }
 
 }
