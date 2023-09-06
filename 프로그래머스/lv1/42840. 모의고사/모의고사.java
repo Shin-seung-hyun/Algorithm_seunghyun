df67bb074ca9c2dafd49acfb74ee90c8617beee7
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-09-06 15:09:25 +0900

    [level 1] Title: 모의고사, Time: 0.69 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -3,33 +3,34 @@ import java.io.*;
 
 class Solution {
     
-    static class Node{
-        int idx;
-        int value;
-    }
-    
     public int[] solution(int[] answers) {
-        
+
+        int[][] pattern = {
+
+            {1,2,3,4,5},
+            {2,1,2,3,2,4,2,5},
+            {3,3,1,1,2,2,4,4,5,5}    
+        };
+                
         int[] score = new int [3];
-        
-        int[]arr1 = {1,2,3,4,5};
-        int[]arr2 = {2,1,2,3,2,4,2,5};
-        int[]arr3 = {3,3,1,1,2,2,4,4,5,5};
 
         for(int i=0; i< answers.length; i++){
             
-            if( answers[i] == arr1[i % 5]) score[0]++;
-            if( answers[i] == arr2[i % 8]) score[1]++;
-            if( answers[i] == arr3[i % 10]) score[2]++;
+            if( answers[i] == pattern[0][i % 5]) score[0]++;
+            if( answers[i] == pattern[1][i % 8]) score[1]++;
+            if( answers[i] == pattern[2][i % 10]) score[2]++;
         }
         
+        // 최댓값  찾기
         int max = Math.max( score[0], Math.max( score[1], score[2]));
         
-        ArrayList<Integer> arrList = new ArrayList<Integer>();
+        // 높은 점수가 여러명인지 확인
+        ArrayList<Integer> arrList = new ArrayList<>();
         for(int i=0; i< 3; i++){
             if( max == score[i]) arrList.add( i+1);
         }
         
+        // 결과값을 출력
         int []result = new int [arrList.size()];
         for(int i=0; i< arrList.size(); i++) result[i] = arrList.get(i);
         
