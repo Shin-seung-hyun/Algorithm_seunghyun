10ff92862529ee5b6b9177d9f691406edcff50f2
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-09-06 16:04:08 +0900

    [level 3] Title: 네트워크, Time: 0.48 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 
-// 인접행렬
+// 인접 리스트
     // BFS
 class Solution {
     
@@ -10,61 +10,51 @@ class Solution {
     static ArrayList<Integer>[] adj;
     
     public int solution(int N, int[][] computers) {
-    
-        
+
         // 초기화
         visit = new boolean[N];
-        adj = new ArrayList[N];
-        for(int i=0; i<N; i++) adj[i] = new ArrayList<>();
-        
-        // 인접 리스트 입력
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                
-                if( computers[i][j] != 1) continue;
-                
-                adj[i].add(j);
-                adj[j].add(i);
-            }
-        }
-        
-        
-        // BFS9 탐색
+
+        // BFS 탐색
         int cnt =0;
         for(int i =0; i<N; i++){
-            for( int j : adj[i]){
-                
+            for( int j =0; j<N; j++){
+
+                if( computers[i][j] !=1) continue;
+
                 if( visit[j]) continue;
-                
-                BFS(j);
-                
+
+                BFS(j, N, computers);
+
                 cnt++;
             }
         }
-        
+
         return cnt;
     }
-    
-    
-    static void BFS(int x){
+
+
+    static void BFS(int x, int N, int[][] computers){
         Queue<Integer> queue = new LinkedList<>();
-        
+
         // 방문
         visit[x] = true;
         queue.add(x);
-        
+
         //탐색
         while(!queue.isEmpty()){
-            
+
             x = queue.poll();
-            
-            for( int val : adj[x]){
-                
-                if( visit[val]) continue;
-                
-                visit[val] = true;
-                queue.add(val);
+
+            for( int i=0; i<N; i++){
+
+                if( computers[x][i] !=1) continue;
+                if( visit[i]) continue;
+
+                visit[i] = true;
+                queue.add(i);
             }
         }
     }
+    
+    
 }
