3ecafdfff30fc79811699c4d74e5876e53cf719f
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-10-18 16:23:13 +0900

    [Silver II] Title: 행복한 소수, Time: 320 ms, Memory: 24540 KB -BaekjoonHub

---
+++ 백준/Silver/10434. 행복한 소수/행복한 소수.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static boolean []check = new boolean[10_000 +1];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        //소수 판별 시작
+        init();
+
+        for(int t=0; t<testCase; t++){
+            st = new StringTokenizer(br.readLine());
+
+            int tNum = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            String answer = "";
+
+            //소수
+            if(check[m]){
+
+                //행복한 수 인지 확인
+                if( happyNum(String.valueOf(m))){
+                    answer = "YES";
+                }
+                else answer = "NO";
+            }
+            else answer = "NO";
+
+            sb.append(tNum + " " + m + " " + answer + "\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+    static void init(){
+
+        Arrays.fill(check, true);
+
+        check[0] = false;
+        check[1] = false;
+        check[2] = true;
+
+        for(int i =3; i<=10_000; i++){
+
+            for(int j = 2; j<= Math.sqrt(i); j++){
+                if( i%j == 0){
+                    check[i] = false;
+                    break;
+                }
+            }
+
+        }
+    }
+
+    static boolean happyNum(String num){
+
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        // 같은 수가 반복될 때까지 연산
+        while(true){
+            int sum = 0;
+            for(int i=0; i< num.length(); i++){
+                sum += Math.pow(Integer.parseInt(String.valueOf(num.charAt(i))),2);
+            }
+
+            //1이 되는지 확인
+            if(sum == 1){
+                return true;
+            }
+
+            //중복 사이클 확인
+            if( hashSet.contains(sum)) break;
+            else hashSet.add(sum);
+
+            num = String.valueOf(sum);
+        }
+
+        return false;
+    }
+}
