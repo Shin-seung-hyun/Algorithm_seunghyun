984fc69c00041a5b443b942e538369c39c6c9832
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-10-27 15:21:07 +0900

    [Gold IV] Title: 알고스팟, Time: 144 ms, Memory: 14704 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,89 +1,93 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
- 
 public class Main {
-    static int[] rangeX = { -1, 0, 1, 0 };
-    static int[] rangeY = { 0, 1, 0, -1 };
-    static int N, M;
-    static int[][] map;
-    
-    static class Point implements Comparable<Point>{
+
+    static StringBuilder sb = new StringBuilder();
+    static int M, N;
+    static int[][] arr;
+    static int[][] dir = { {1,0},{0,1},{-1,0},{0,-1}};
+    static boolean[][] visit;
+
+    // 자기 자신과 비교 vs 다른 객체끼리 비교
+        // Comparable vs Comparator
+        // compareTo() vs compare()
+    static class Node implements Comparable<Node> {
         int x;
         int y;
-        int cnt; // 벽은 부순 개수
-        
-        public Point(int x, int y, int cnt ){
+        int cnt ; // 부순 벽의 수
+
+        public Node(int x,int y, int cnt){
             this.x = x;
             this.y = y;
             this.cnt = cnt;
         }
-        
+
         @Override
-        public int compareTo(Point o){
+        public int compareTo(Node o){
             return this.cnt - o.cnt;
         }
     }
-    
-    
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        M = Integer.parseInt(st.nextToken()); // 가로
-        N = Integer.parseInt(st.nextToken()); // 세로
- 
-        map = new int[N + 1][M + 1];
- 
-        for (int i = 1; i <= N; i++) {
-            String input = br.readLine();
-            for (int j = 1; j <= M; j++) {
-                map[i][j] = Character.getNumericValue(input.charAt(j - 1));
+
+        N = Integer.parseInt(st.nextToken());   // 열
+        M = Integer.parseInt(st.nextToken());   // 행
+
+        //입력
+        arr = new int [M][N];
+        visit = new boolean[M][N];
+
+        for(int i = 0; i< M; i++){
+            String tmp = br.readLine();
+            for(int j = 0; j< N; j++){
+                arr[i][j] = tmp.charAt(j)- '0';
             }
         }
- 
-        int ans = BFS(1, 1);
- 
-        System.out.println(ans + "\n");
+
+        BFS(0,0);
     }
- 
-    public static int BFS(int x, int y) {
-        // 벽을 부순 개수를 오름차순 정렬
-        PriorityQueue<Point> q = new PriorityQueue<>();
- 
-        q.offer(new Point(x, y, 0));
-        boolean[][] visit = new boolean[N + 1][M + 1];
-        visit[x][y] = true;
- 
-        int dx, dy;
-        while (!q.isEmpty()) {
-            Point p = q.poll();
- 
-            // 도착점에 도달하면 종료
-            if (p.x == N && p.y == M) {
-                return p.cnt;
+
+    static void BFS(int sx ,int sy){
+
+        //벽 부순 개수를 오름차순 정렬 -> 제일 작은 벽 부순 수로 탐색
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+        visit[sx][sy] = true;
+        pq.add(new Node( sx,sy,0));
+
+        while(!pq.isEmpty()){
+
+            Node curNode = pq.poll();
+
+            if( curNode.x == M -1 && curNode.y == N -1){
+                System.out.println( curNode.cnt );
+                return;
             }
- 
-            for (int i = 0; i < 4; i++) {
-                dx = p.x + rangeX[i];
-                dy = p.y + rangeY[i];
- 
-                if (dx < 1 || dy < 1 || dx > N || dy > M) {
-                    continue;
+
+            for(int i= 0; i<4; i++){
+                int nx = curNode.x + dir[i][0];
+                int ny = curNode.y + dir[i][1];
+
+                if( nx <0 || ny<0 || nx >= M || ny >= N) continue;
+
+                if( visit[nx][ny]) continue;
+
+                // 벽이 없다면
+                if( arr[nx][ny] == 0){
+
+                    pq.add( new Node(nx,ny, curNode.cnt));
                 }
- 
-                if (!visit[dx][dy] && map[dx][dy] == 0) {
-                    visit[dx][dy] = true;
-                    q.offer(new Point(dx, dy, p.cnt));
+                //벽이 있다면
+                else if ( arr[nx][ny] == 1){
+                    pq.add( new Node(nx,ny,curNode.cnt+1));
                 }
- 
-                if (!visit[dx][dy] && map[dx][dy] == 1) {
-                    visit[dx][dy] = true;
-                    q.offer(new Point(dx, dy, p.cnt + 1));
-                }
+
+                visit[nx][ny] = true;
             }
         }
-        return 0;
     }
- 
+
 }
