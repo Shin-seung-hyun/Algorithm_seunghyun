b96e4951752fc5346f92062190f8ba3f726978ef
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2023-11-03 23:53:53 +0900

    [Silver II] Title: 약수의 합 2, Time: 232 ms, Memory: 17816 KB -BaekjoonHub

---
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) {
+		int N;
+		long res = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		
+		for(int i = 1; i <= N; i++) {
+			res += i * (N/i);
+		}
+		System.out.println(res);
+	}
+}
