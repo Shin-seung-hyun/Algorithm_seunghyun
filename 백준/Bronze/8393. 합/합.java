275dbf73717af3e26314c0415ad13870cc53f5da
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-01-03 16:22:18 +0900

    [Bronze V] Title: 합, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        
+        int sum =0;
+        for(int i=1; i<=n; i++) sum+=i;
+        System.out.println(sum);
+
+    }
+}
