88e660904e96a9942ddcf7156205956f172f9976
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-01-03 14:49:41 +0900

    [Bronze V] Title: A+B - 7, Time: 160 ms, Memory: 16052 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int testCase = Integer.parseInt(br.readLine());
+        
+        for(int i=0; i<testCase; i++){
+            
+            st = new StringTokenizer(br.readLine());
+            
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            
+            System.out.println( "Case #"+ (i+1) + ": " + (A+B) );
+        }
+
+    }
+}
