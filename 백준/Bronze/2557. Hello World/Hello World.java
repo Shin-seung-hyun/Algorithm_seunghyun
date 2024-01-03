791c6bd6bd524579cd9b44efc905988e8640701a
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-01-03 13:54:36 +0900

    [Bronze V] Title: Hello World, Time: 112 ms, Memory: 13844 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,5 +1,9 @@
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args){
+        
+        System.out.println("Hello World!");
+    }
 }
