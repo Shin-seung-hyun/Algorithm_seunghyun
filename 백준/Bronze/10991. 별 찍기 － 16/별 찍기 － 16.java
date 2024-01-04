4f6539874fdf09f1425202f0d98e26b5535158c1
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-01-04 21:36:59 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 128 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        
+        for(int i=1; i<=N; i++){
+            
+            for(int j=1; j<= N-i; j++) sb.append(" ");
+            for(int j=1; j<=i; j++)sb.append("* ");
+             
+            sb.append("\n");
+        }
+       
+       System.out.print(sb.toString());
+    }
+}
