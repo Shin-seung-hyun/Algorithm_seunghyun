be6eca297140097f3d112386a6b815c53b48d993
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-01-08 14:48:49 +0900

    [Silver I] Title: 정수 삼각형, Time: 292 ms, Memory: 29264 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,47 +1,55 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
+
+//참고 : https://st-lab.tistory.com/131
 class Main{
+
     static int N;
-    static int [][]arr;
-    static int [][]Dy;
+    static int[][]arr;
+    static Integer[][]Dy;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
         N = Integer.parseInt(br.readLine());
 
-        arr = new int[N+1][N+1];
-        Dy = new int[N+1][N+1];
+        arr = new int[N+1][N+1];    // int[][] 배열의 초기값 0
+        Dy = new Integer[N+1][N+1]; // Integer[][] 배열의 초기값 null
 
-        for(int i=1; i<=N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j =1; j<=i; j++){
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            for (int j = 1; j <= i; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        DP();
+        //초기값
+            //Dy의 마지막 행 채우기
+        for(int i= 1; i<=N; i++) Dy[N][i] = arr[N][i];
 
-        // 가장 마지막 줄에서 가장 큰 값 찾기
-        int answer =0;
-        for(int i =1; i<=N; i++){
-            answer = Math.max(answer, Dy[N][i]);
-        }
 
-        System.out.println(answer);
+        System.out.println( find(1,1));
     }
 
-    static void DP(){
+    static int find(int depth, int idx){
 
-        // 점화식
-        //( i , j )가 선택되기 전의 왼쪽 위 ( i-1, j-1), 오른쪽 위 ( i-1, j )
-        // Dy[i][j] = Max(Dy[i-1][j] , Dy[i-1][j-1] ) + arr[i][j]
+        //종료 조건
+            // 마지막 행일 경우 현재 위치의 Dy값 반환
+        if(depth == N) return Dy[depth][idx];
 
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=N; j++){
+        //반복 조건
+            //탐색하지 않았던 값일 경우, 다음 행의 양쪽 값 비교
+        if( Dy[depth][idx] == null){
 
-                Dy[i][j] = Math.max(Dy[i-1][j-1] , Dy[i-1][j]) + arr[i][j];
-            }
+            Dy[depth][idx] = Math.max( find(depth +1, idx),
+                                       find( depth+1, idx+1) ) + arr[depth][idx];
         }
+
+        return Dy[depth][idx];
     }
+
+
 }
