ce9f60a22efc542b1800d5c5860b6ce5d635388a
Shin-seung-hyun <jjack1028@naver.com>
2024-01-10 15:47:41 +0900

    add : 퇴사 풀이 추가

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,8 +1,12 @@
 import java.util.*;
 import java.io.*;
 
-
-// 가장 긴 증가하는 부분 수열
+/*
+    완전탐색 O(2^N)
+    DP O(N^2)
+    이분탐색 O(Nlog^N)
+    투 포인터 O(N)
+*/
 class Main{
     static int N;   // 수열의 길이
     static int[] arr; // 수열을 저장
@@ -21,19 +25,22 @@ class Main{
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        DP();
 
-        int max = 0;
-        for(int i : Dy){
-            max = Math.max( max, i);
-        }
-        System.out.println(max);
-    }
-
-    static void DP(){
-        //초기값
+        //가짜식 Dy[i] : i가 제일 큰 수일 때, 가장 긴 증가하는 부분 수열의 길이
+        //초기화
         Dy[1] = 1;
 
+        //점화식1
+        for(int i=2; i<=N; i++){
+            for(int j=1; j<i; j++){
+
+                if( arr[i] <= arr[j]) continue;
+                Dy[i] = Math.max(Dy[j], Dy[i]);
+            }
+            Dy[i] +=1 ;
+        }
+
+        //점화식2
         for(int i =2; i<=N; i++){
             Dy[i] = 1;
 
@@ -44,7 +51,9 @@ class Main{
             }
         }
 
+        int cnt= 0;
+        for( int val : Dy) cnt = Math.max(cnt, val);
+
+        System.out.println(cnt);
     }
-
-
 }
--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,39 +1,44 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//DP -> O(N)
+class Main{
+	public static void main(String[]args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-        
-        int n = Integer.parseInt(reader.readLine());
-		
-		int[] t = new int[n];
-		int[] p = new int[n];
 
-		for (int i=0; i<n; i++) {
-			st = new StringTokenizer(reader.readLine());
-			
-			t[i] = Integer.parseInt(st.nextToken());
-			p[i] = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(br.readLine());
+
+		// Dy[i] : i-1 날까지 상담했을 때, 최대 이익임으로 +2
+		int [] Dy = new int[15+2];
+		int[] time = new int[15+1];
+		int[] price = new int[15+1];
+
+		for(int i=1; i<=N; i++){
+			st = new StringTokenizer(br.readLine());
+
+			time[i] = Integer.parseInt(st.nextToken());
+			price[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		int[] dp = new int[n+1]; //N일에 얻을 수 있는 최대 수익
-		
-		//점화식
-		    //현재 날짜에서 소요 시간과 비용을 더해 dp에 저장
-		    //이후, 중복될 때 최대값을 넣는다.
-		for (int i=0; i<n; i++) {
-			if (i + t[i] <= n) {
-				dp[i + t[i]] = Math.max(dp[i + t[i]], dp[i] + p[i]);
+
+		// 가짜식 Dy[i] : i-1 날까지 상담했을 때, 최대 이익
+		// 초기화
+
+		// 점화식
+		for(int i = 1; i <= N; i++) {
+
+			//Dy[i] 값 확정
+			Dy[i] = Math.max(Dy[i], Dy[i-1]);
+
+			//Dy[i + time[i]] 값 갱신
+			// i번째 날 상담을 할 경우, i+time[i]-1은 상담이 종료되는 날
+			if(i+time[i]-1 <= N) {
+
+				// max(원래 있던 값, 갱신될 값 )
+				Dy[i + time[i]] = Math.max( Dy[i + time[i]], Dy[i] + price[i]);
 			}
-
-			dp[i+1] = Math.max(dp[i+1], dp[i]);
 		}
-        
-		System.out.println(dp[n]);
-		
+
+		System.out.println(Math.max(Dy[N], Dy[N+1]));
 	}
 }
