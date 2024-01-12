d1430252a583644fc3b3aa5b81a9ee2e0da246c3
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-01-12 20:04:39 +0900

    [Silver I] Title: 포도주 시식, Time: 148 ms, Memory: 15256 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,49 +1,50 @@
 import java.util.*;
+import java.io.*;
 
-//동적 프로그래밍(DP)
+/*
+    DP
+*/
 class Main{
 
-    static int N;
-    static int[] Dy;
-    static int[] arr;
+    public static void main(String[]args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(br.readLine());
 
-        N = sc.nextInt();
-        arr = new int[N+1];
-        Dy = new int[N+1];
+        int [][] dy = new int[10_000+1][3];
+        int [] arr = new int[10_000+1];
 
-        for(int i =1; i<=N;i++){
-            arr[i] = sc.nextInt();
+        for(int i=1; i<=N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        DP();
 
-        System.out.println(Dy[N]);
-    }
+        /*
+            경우의 수
+            N-3     N-2     N-1     N
+                             X      X
+                             O      X
+                    O        O      X
+                             X      O
+                     X       O      O
+        */
 
-    static void DP(){
-        // 초기값 구하기
-        Dy[1] = arr[1];
+        //가짜식 dy[i][0] :  i번째 안 마신 경우
+        //      dy[i][1] :  i번째 마시고, 연속으로 1번 먹은 경우
+        //      dy[i][2] :  i번째 마시고, 연속으로 2번 먹은 경우
+        //초기화
+        dy[1][1] = arr[1];
 
-        if (N > 1){
-            Dy[2] = arr[1] + arr[2];
+        //점화식
+        for(int i =2; i<=N; i++){
+
+            dy[i][0] = Math.max(dy[i-1][0], Math.max( dy[i-1][1], dy[i-1][2]) );
+            dy[i][1] = dy[i-1][0] + arr[i];
+            dy[i][2] = dy[i-1][1] + arr[i];
         }
 
-        // 점화식을 토대로 Dy 배열 채우기
-        for (int i = 3; i <= N; i++){
-
-            /*
-                경우의 수
-                N-3     N-2     N-1     N
-                                 O      X
-                         O       X      O
-                O        X       O      O
-            */
-            Dy[i] = Math.max( Dy[i-1],
-                    Math.max( Dy[i-2] + arr[i], Dy[i-3] + arr[i-1] + arr[i]));
-        }
-
+        System.out.println( Math.max(dy[N][0], Math.max( dy[N][1],dy[N][2]) ) );
     }
 }
