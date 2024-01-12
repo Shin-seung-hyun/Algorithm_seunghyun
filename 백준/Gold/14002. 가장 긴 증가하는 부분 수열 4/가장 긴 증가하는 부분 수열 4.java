4ef7c334fc6506a9fe93f3afc942a4eeb9010b07
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-01-12 14:50:35 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 176 ms, Memory: 17376 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+
+/*
+
+가장 긴 증가하는 부분 수열 + 수열 출력
+
+    완전탐색 O(2^N)
+    DP O(N^2)
+    이분탐색 O(Nlog^N)
+    투 포인터 O(N)
+
+*/
+class Main{
+
+    public static void main(String[]args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int [] dy = new int[N+1];
+        int [] arr = new int[N+1];
+        int [] pre = new int[N+1];  // 어디서 왔는지 저장하는 배열
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        //가짜식 dy[i] : i가 제일 큰 수일 때, 가장 긴 증가하는 부분 수열의 길이
+        //초기화
+        dy[1] = 1;
+
+        //점화식
+        for(int i=2; i<=N; i++){
+            for(int j=1; j<i; j++){
+
+                if( arr[j] < arr[i] && dy[i] < dy[j]){
+                    dy[i] = dy[j];
+                    pre[i] = j;
+                }
+            }
+            dy[i] += 1;
+        }
+
+    //정답 출력
+        // 수열의 길이
+        int answer = 0;
+        int idx =0;
+        for(int i=1; i<=N; i++){
+
+            if(answer <dy[i]){
+                answer = dy[i];
+                idx = i;
+            }
+        }
+
+        //수열 출력
+        int[] result = new int[answer];
+        int i = result.length-1;
+        while(true){
+
+            if(idx ==0) break;
+
+            result[i] = arr[idx];
+            i--;
+
+            idx = pre[idx];
+        }
+
+        System.out.println(answer);
+        for(int val : result) System.out.print(val + " ");
+    }
+}
