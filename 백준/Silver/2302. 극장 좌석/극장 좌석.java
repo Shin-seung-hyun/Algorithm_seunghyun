efdcca19a8780addcbe77f87305080b8003e04a3
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-01-15 16:34:35 +0900

    [Silver I] Title: 극장 좌석, Time: 128 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.java
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.java
@@ -12,7 +12,8 @@ class Main{
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());    //좌석 수
-
+        int M = Integer.parseInt(br.readLine());    //고정석
+        
         int [] dy = new int[40 +1];
 
         //가짜식 dy[i] : 극장 자리가 i개 일때의 좌석 이용 방법 수
@@ -26,27 +27,38 @@ class Main{
             dy[i] = dy[i-1] + dy[i-2];
         }
 
-        //정답
-        int M = Integer.parseInt(br.readLine());    //고정석
-        int answer = dy[N];
-
-        if( M >= 1 ) {
-            int pre = Integer.parseInt(br.readLine());
-            answer = dy[pre - 1];
-
-            if (M >= 2) {
-                for (int i = 2; i <= M; i++) {
-                    int num = Integer.parseInt(br.readLine());
-
-                    answer *= dy[num - pre - 1];
-
-                    pre = num;
-                }
-            }
-
-            if ( pre < N)
-                answer *= dy[N - pre];
+    //정답 구하기
+        //[방법1]
+//        int answer = dy[N];
+//
+//        if( M >= 1 ) {
+//            int pre = Integer.parseInt(br.readLine());
+//            answer = dy[pre - 1];
+//
+//            if (M >= 2) {
+//                for (int i = 2; i <= M; i++) {
+//                    int num = Integer.parseInt(br.readLine());
+//
+//                    answer *= dy[num - pre - 1];
+//
+//                    pre = num;
+//                }
+//            }
+//
+//            if ( pre < N)
+//                answer *= dy[N - pre];
+//        }
+        
+        //[방법2]
+        int answer = 1;
+        int pre = 0;
+        for(int i=0; i<M; i++){
+            int n = Integer.parseInt(br.readLine());
+            
+            answer *= dy[n - pre -1];
+            pre = n;
         }
+        answer *= dy[N-pre];
 
         System.out.println(answer);
     }
