c52dddbba6d0d3c5f042d8deb15f2b6351afbc35
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-02-21 15:26:49 +0900

    [Bronze II] Title: 숫자의 개수, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,26 +1,38 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
+class Main{
+    public static void main(String args[]) throws IOException{
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
 
-        int num = A * B * C;
-        String temp = String.valueOf(num);
-        int result [] = new int[10];
+        int num  = A * B * C;
+        int numArr[] = new int[10];
 
-        for(int i = 0; i <temp.length(); i++){
+        //1. num을 연산으로 하나씩 떨구기
+        // 4325
+//        while( num > 10){
+//            int idx = num % 10;
+//            numArr[idx]++;
+//
+//            num = num/10;
+//        }
+//        numArr[num]++;
 
-            result[temp.charAt(i) -'0'] += 1 ;
+
+        //2. String으로 만들어서 하나씩 떨구기
+        String str = String.valueOf(num);
+
+        for(int i =0; i< str.length(); i++){
+            numArr[ str.charAt(i) - '0']++;
         }
 
-        for(int i = 0; i< result.length; i++){
-            System.out.println(result[i]);
-        }
-        
+        //출력
+        for(int i =0; i<10; i++) System.out.println(numArr[i]);
+
     }
 }
