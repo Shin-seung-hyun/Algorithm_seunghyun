e1fcb26a3e86807847813bd114a538f77a40b4d4
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-02-23 19:49:32 +0900

    [Silver IV] Title: 스택, Time: 196 ms, Memory: 19580 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,87 +1,57 @@
-import java.util.Scanner;
-public class Main {
- 
-	public static int[] stack;
-	public static int size = 0;
- 
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		
-		int N = in.nextInt();
- 
-		stack = new int[N];
-		
-		for(int i = 0; i < N; i++) {
-        
-			String str = in.next();
- 
-			switch (str) {
-            
-			case "push":
-				push(in.nextInt());
-				break;
-                
-			case "pop":
-				sb.append(pop()).append('\n');
-				break;
-                
-			case "size":
-				sb.append(size()).append('\n');
-				break;
-                
-			case "empty":
-				sb.append(empty()).append('\n');
-				break;
-                
-			case "top":
-				sb.append(top()).append('\n');
-				break;
-			}
- 
-		}
-		System.out.println(sb);
-	}
- 
-	public static void push(int item) {
-		stack[size] = item;
-		size++;
-	}
-    
-	public static int pop() {
-		if(size == 0) {
-			return -1;
-		}
-		else {
-			int res = stack[size - 1];
-			stack[size - 1] = 0;
-			size--;
-			return res;
-		}
-	}
-    
-	public static int size() {
-		return size;
-	}
-    
-	public static int empty() {
-		if(size == 0) {
-			return 1;
-		}
-		else {
-			return 0;
-		}
-	}
-    
-	public static int top() {
-		if(size == 0) {
-			return -1;
-		}
-		else {
-			return stack[size - 1];
-		}
-	}
-	
+import java.util.*;
+import java.io.*;
+
+//Stack
+    // 스택을 구현하는 방식에는 Array(배열), LinkedList(연결 리스트) 2가지가 있다.
+class Main{
+    public static void main(String[] args) throws  IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        //배열로 stack 구현하기
+        int[] stack = new int[N];
+        int idx =0; // 다음 원소를 추가할 곳을 가리킴.
+
+        for(int i=1; i<=N; i++){
+            st = new StringTokenizer(br.readLine());
+
+            String op = st.nextToken();
+            switch (op){
+                case "push" :
+                    int num = Integer.parseInt(st.nextToken());
+                    stack[idx] = num;
+                    idx++;
+                    break;
+
+                case "pop" :
+                    if(idx == 0) sb.append("-1").append("\n");
+                    else {
+                        idx--;
+                        sb.append(stack[idx]).append("\n");
+                    }
+                    break;
+
+                case "size" :
+                    sb.append(idx).append("\n");
+                    break;
+
+                case "empty" :
+                    if( idx == 0) sb.append("1").append("\n");
+                    else sb.append("0").append("\n");
+                    break;
+
+                case "top" :
+                    if( idx ==0) sb.append("-1").append("\n");
+                    else sb.append(stack[idx-1]).append("\n");
+                    break;
+            }
+        }
+
+        //출력
+        System.out.print(sb.toString());
+
+    }//end main
 }
