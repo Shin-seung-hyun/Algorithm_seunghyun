354240e8d4f46a1bc6c449fb32271befc19ac728
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-02-25 22:16:55 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 284 ms, Memory: 23524 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -4,15 +4,6 @@ import java.io.*;
 //Stack
 public class Main {
 
-    static class Building{
-        int idx,height;
-
-        public Building(int idx, int height){
-            this.idx= idx;
-            this.height = height;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -20,28 +11,44 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());   // 빌딩수
 
-        Stack<Building> stack = new Stack<>();
-
+        Stack<Integer> stack = new Stack<>();
+        
+    //방법1.
         // 79_999 + ... + 1 ~= 32억임으로 long
         long sum = 0;
         for(int i=1; i<=N; i++){
-            int h = Integer.parseInt(br.readLine());
-
-            while(!stack.isEmpty() && stack.peek().height <= h){
-                sum += i - stack.peek().idx -1;
+            int height = Integer.parseInt(br.readLine());
+            
+            // 제일 큰 빌딩 높이를 만났을 때, stack pop
+                // 중복일 때도 옥상을 볼 수 없음.
+            while(!stack.isEmpty() && stack.peek() <= height){
                 stack.pop();
             }
-
-            stack.push(new Building(i,h));
+            sum += stack.size();
+            
+            stack.push(height);
         }
-
-
-        // 남아 있는 스택 처리
-        while(!stack.isEmpty()){
-            sum += N - stack.peek().idx;
-            stack.pop();
-        }
-
+        
         System.out.println(sum);
+
+    //방법2. sum 계산
+//        for(int i=1; i<=N; i++){
+//            int height = Integer.parseInt(br.readLine());
+//
+//            while(!stack.isEmpty() && stack.peek() <= height){
+//                sum += i - stack.peek() -1;
+//                stack.pop();
+//            }
+//            stack.push(height);
+//        }
+//        System.out.println(sum);
+//        
+//        // 남아 있는 스택 처리
+//        while(!stack.isEmpty()){
+//            sum += N - stack.peek();
+//            stack.pop();
+//       }
+//
+//        System.out.println(sum);
     }
 }
