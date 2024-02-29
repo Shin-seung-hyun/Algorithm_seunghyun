394200752bfb36b3b191f21af3eeb80a9f80afaa
Shin-seung-hyun <jjack1028@naver.com>
2024-02-29 23:33:11 +0900

    add : AC 풀이 추가

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -19,53 +19,62 @@ public class Main {
             // 배열에 들어 있는 수
             int N = Integer.parseInt(br.readLine());
 
+            /* 방법1 */
+            //str.substring(beginIdx 포함, endIdx 미포함)
             String str = br.readLine();
-            String[] arr = str.substring(1, str.length()-1).split(","); //str.substring(beginIdx 포함, endIdx 미포함)
+            String[] arr = str.substring(1, str.length()-1).split(",");
 
             LinkedList<Integer> dq = new LinkedList<>();
-
             for(int i=0; i<N; i++) dq.add( Integer.parseInt(arr[i]));
 
-            boolean isReverse = false;
-            boolean isError = false;
-            for(char op : p){
+            /* 방법2 */
+            st = new StringTokenizer(br.readLine(), "[],");
 
-                if(op == 'R'){  //R : 순서를 뒤집기
-                    isReverse = !isReverse;
-                }
-                else{ //op == 'D, 첫 번째 수를 버리기, 단, 비어 있으면 error 출력
+            LinkedList<Integer> dq = new LinkedList<>();
+            for(int i = 0; i < N; i++) dq.add(Integer.parseInt(st.nextToken()));
+        }
 
-                    if( dq.isEmpty()){
-                        isError = true;
-                        break;
-                    }
-                    else{
-                        if( isReverse) dq.removeLast();
-                        else dq.removeFirst();
-                    }
 
-                }
+        boolean isReverse = false;
+        boolean isError = false;
+        for(char op : p){
+
+            if(op == 'R'){  //R : 순서를 뒤집기
+                isReverse = !isReverse;
             }
+            else{ //op == 'D, 첫 번째 수를 버리기, 단, 비어 있으면 error 출력
 
-            //출력
-            if(isError)sb.append("error\n");
-            else {
-
-                sb.append("[");
-                while(!dq.isEmpty()) {
-
-                    if (isReverse) sb.append(dq.removeLast());
-                    else sb.append(dq.removeFirst());
-                    
-                    if(dq.size()!=0)
-                        sb.append(",");
+                if( dq.isEmpty()){
+                    isError = true;
+                    break;
                 }
-                //if( sb.charAt(sb.length()-1) == ',') sb.deleteCharAt( sb.length()-1);
-                sb.append("]\n");
-            }
+                else{
+                    if( isReverse) dq.removeLast();
+                    else dq.removeFirst();
+                }
 
-        }// end testCase
+            }
+        }
+
+        //출력
+        if(isError)sb.append("error\n");
+        else {
+
+            sb.append("[");
+            while(!dq.isEmpty()) {
+
+                if (isReverse) sb.append(dq.removeLast());
+                else sb.append(dq.removeFirst());
+
+                if(dq.size()!=0)
+                    sb.append(",");
+            }
+            //if( sb.charAt(sb.length()-1) == ',') sb.deleteCharAt( sb.length()-1);
+            sb.append("]\n");
+        }
+
+    }// end testCase
 
         System.out.print(sb.toString());
-    }
+}
 }
