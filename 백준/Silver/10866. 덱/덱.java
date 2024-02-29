4ef76265641e8eb350bc1c54db3e5db94ca298e9
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-02-29 19:49:04 +0900

    [Silver IV] Title: 덱, Time: 204 ms, Memory: 18972 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -1,14 +1,12 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.ArrayDeque;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		ArrayDeque<Integer> dq = new ArrayDeque<Integer>();
+		Deque<Integer> dq = new ArrayDeque<Integer>();
 		StringBuilder sb = new StringBuilder();
 
 		int N = Integer.parseInt(br.readLine());
