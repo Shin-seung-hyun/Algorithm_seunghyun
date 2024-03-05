5eefbb1c1b2da5157ef129cea7189a3536038beb
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-03-05 13:51:54 +0900

    [Gold IV] Title: 불!, Time: 596 ms, Memory: 71184 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -1,7 +1,8 @@
 import java.io.*;
 import java.util.*;
 
-
+//BFS()
+    // 불이 한 번 움직이고, 지훈이가 한 번 움직일 때
 public class Main {
     static class Pos{
         int x;
@@ -13,81 +14,110 @@ public class Main {
         }
     }
     static int N, M;
-    static char[][] map;
+    static char[][] adj;
+
     static Queue<Pos> jihunQ = new LinkedList<>();
     static Queue<Pos> fireQ = new LinkedList<>();
-    static boolean[][] jihunVisited;
-    static boolean[][] fireVisited;
+
+    static boolean[][] jihun_Visited;
+    static boolean[][] fire_Visited;
+
+    static int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new char[N][M];
-        jihunVisited = new boolean[N][M];
-        fireVisited = new boolean[N][M];
+
+        adj = new char[N][M];
+        jihun_Visited = new boolean[N][M];
+        fire_Visited = new boolean[N][M];
+
         for(int i=0; i<N; i++) {
             String str = br.readLine();
+
             for(int j=0; j<M; j++) {
-                map[i][j] = str.charAt(j);
-                if(map[i][j] == 'J') {
-                    map[i][j] = '.';
+                char c = str.charAt(j);
+
+                if( c == 'J') {
+                    adj[i][j] = '.';
                     jihunQ.add(new Pos(i, j));
-                    jihunVisited[i][j] = true;
-                } else if(map[i][j] == 'F') {
-                    fireQ.add(new Pos(i, j));
-                    fireVisited[i][j] = true;
+                    jihun_Visited[i][j] = true;
                 }
+
+                else if( c == 'F'){
+                    fireQ.add(new Pos(i, j));
+                    fire_Visited[i][j] = true;
+                }
+                
+                adj[i][j] = c;
             }
         }
 
-        bfs();
+        BFS();
+
         System.out.println("IMPOSSIBLE");
     }
 
-    private static void bfs() {
-        int[][] dist = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private static void BFS() {
+
         int time = 0;
 
+        // 지훈이가 더이상 갈 곳이 없을 때까지
         while(!jihunQ.isEmpty()) {
             int jihunLen = jihunQ.size();
             int fireLen = fireQ.size();
+
+        //1. 불
             for(int i=0; i<fireLen; i++) {
                 Pos fire = fireQ.poll();
-                for(int d=0; d<4; d++) {
-                    int nx = fire.x + dist[d][0];
-                    int ny = fire.y + dist[d][1];
 
-                    if(!isIn(nx, ny) || map[nx][ny] == '#' || fireVisited[nx][ny]) continue;
-                    fireVisited[nx][ny] = true;
-                    map[nx][ny] = 'F';
-                    fireQ.add(new Pos(nx, ny));
+                for(int j=0; j<4; j++) {
+                    int nfx = fire.x + dir[j][0];
+                    int nfy = fire.y + dir[j][1];
+
+                    if(nfx <0 || nfx >= N || nfy <0 || nfy >=M) continue;
+                    if(adj[nfx][nfy] == '#') continue;
+                    if(fire_Visited[nfx][nfy]) continue;
+                    
+                    fireQ.add(new Pos(nfx, nfy));
+                    fire_Visited[nfx][nfy] = true;
+                    adj[nfx][nfy] = 'F';
                 }
             }
+
+        //2. 지훈
             for(int i=0; i<jihunLen; i++) {
                 Pos jihun = jihunQ.poll();
-                for(int d=0; d<4; d++) {
-                    int nx = jihun.x + dist[d][0];
-                    int ny = jihun.y + dist[d][1];
 
-                    if(!isIn(nx, ny)) {
+                for(int j=0; j<4; j++) {
+                    int njx = jihun.x + dir[j][0];
+                    int njy = jihun.y + dir[j][1];
+                    
+                    // 종료 조건 - 지훈이가 외각에 도착하면 프로그램 종료
+                    if(njx <0 || njx >= N || njy <0 || njy >=M){
                         time++;
                         System.out.println(time);
+
+                        // 프로그램 종료
+                            // 1. return; -> 일반 함수에서 사용 시, 일반함수만 종료
+                            // 2. System.exit(0); -> 일반 함수에서 사용해도 main함수까지 전체 강제 종료
                         System.exit(0);
                     }
 
-                    if(map[nx][ny] !='.' || jihunVisited[nx][ny]) continue;
-                    jihunVisited[nx][ny] = true;
-                    jihunQ.add(new Pos(nx, ny));
+                    if(adj[njx][njy] !='.' ) continue;
+                    if(jihun_Visited[njx][njy]) continue;
+
+                    jihunQ.add(new Pos(njx, njy));
+                    jihun_Visited[njx][njy] = true;
                 }
             }
-
+            
+        //3.불과 지훈이 각각 한 번씩 움직였다면, 시간 ++ 
             time++;
-        }
-    }
-
-    private static boolean isIn(int x, int y){
-        return 0<=x && x<N && 0<=y && y<M;
+            
+        } // end while
     }
 }
