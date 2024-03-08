c204934abc184c228b326888aa5625790f1c09a8
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-03-08 15:02:06 +0900

    [Silver I] Title: 곱셈, Time: 120 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+//재귀
+class Main{
+
+    static long A,B,C;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        A = Long.parseLong(st.nextToken());
+        B = Long.parseLong(st.nextToken());
+        C = Long.parseLong(st.nextToken());
+
+        //System.out.println(func(B));
+
+        // func() 코드를 개선
+        System.out.println(POW(A,B,C));
+    }
+
+    static long POW(long a, long b ,long c){
+
+        if( b ==1) return a % c;
+
+        long temp = POW(a,b/2,c);
+
+        /*
+            int 범위 : 2^31 -1
+            long 범위 : 2^63 -1
+
+            즉, temp = 2^31 -1 이면, (2^31-1 * 2^31-1) < 2^63-1 이지만
+               a까지 2^31-1이라면  (2^31-1 * 2^31-1 * 2^31-1) > 2^63-1로 long의 범위를 넘는다.
+            따라서  temp * temp % C 를 해야 한다.
+        */
+        if( b % 2 == 1) return ((temp * temp)%c * a) % c;
+        else return temp * temp % c;
+    }
+
+    static long func(Long b){
+
+    // 종료 조건
+        if(b <= 1) {
+            return 1;
+        }
+
+    // 반복 조건
+        // 짝수일 때
+        if(b % 2 ==0){
+            return (long)(Math.pow(A, func(b / 2)) * (Math.pow(A, func(b / 2))  % C));
+        }
+        // 홀수일 때 -> 단, (pow * pow * A % C) 가 아니라 ( (pow * pow %C) *A) % C여야 함
+        else {
+            return (long)((Math.pow(A, func( b / 2 )) * Math.pow(A, func(b / 2))) % C) * A % C;
+        }
+
+    }
+}
