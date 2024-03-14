a0fb0e7d3b5786982ada971af86ac76ab1757020
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-03-14 23:42:13 +0900

    [Silver III] Title: N과 M (2), Time: 204 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -2,73 +2,50 @@ import java.util.Scanner;
 
 //N개 중 M개를 중복 x, 순서 x 고르기
 public class Main {
- 
-	public static int[] arr;
-	public static int N, M;
-	public static StringBuilder sb = new StringBuilder();
 
-	static boolean[] visit;
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		N = in.nextInt();
-		M = in.nextInt();
- 
-		arr = new int[M+1];
-		//visit = new boolean[N+1];
+    public static int[] arr;
+    public static boolean[] visit;
+    public static int N, M;
+    public static StringBuilder sb = new StringBuilder();
 
-		// 방법1
-		dfs(1, 1);
+    public static void main(String[] args) {
 
-		// 방법2
-		//DFS(1, 1);
+        Scanner in = new Scanner(System.in);
 
-		System.out.println(sb.toString());
-	}
+        N = in.nextInt();
+        M = in.nextInt();
 
-	// 방법1
-	public static void dfs(int at, int depth) {
+        arr = new int [M+1];
+        visit  = new boolean[N+1];
 
-		//재귀 종료 조건
-		if(depth == M+1){
-			for(int i =1; i<=M; i++){
-				sb.append(arr[i]).append(" ");
-			}
-			sb.append("\n");
-			return ;
-		}
+        DFS(1,1);
+
+        System.out.print(sb.toString());
+    }
+
+    public static void DFS(int depth, int at) {
+
+        // 종료 조건
+        if( depth == M+1){
+            for(int i=1; i<=M; i++){
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+
+            return;
+        }
         
-		for (int i = at; i <= N; i++) {
- 
-			arr[depth] = i;
-			dfs(i + 1, depth + 1);
- 
-		}
-	}
+        // 반복 조건
+        for(int i =at; i<=N; i++){
 
-	// 방법2
-	static void DFS(int at , int depth){
+            if(visit[i]) continue;
 
-		// 종료 조건
-		if( depth == M + 1){
-			for(int i =1; i<=M; i++){
-				sb.append(arr[i]).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
+            visit[i] = true;
+            arr[depth] = i;
+            DFS(depth +1, i);
+            visit[i] = false;
+        }
 
-		//반복조건
-		for(int i = at; i<=N; i++){
-			if(visit[i]) continue;
+    }
 
-			visit[i] = true;
-			arr[depth] = i;
-			DFS(i, depth+1);
-			visit[i] = false;
-		}
-
-	}
 }
