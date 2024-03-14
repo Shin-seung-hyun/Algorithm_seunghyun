62155edf9734e4e07ea439c533783e81d01a3375
Shin-seung-hyun <jjack1028@naver.com>
2024-03-14 20:00:42 +0900

    refactor : N과 M(1) 코드 리팩토링

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,37 +1,41 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-//N개 중 M개를 중복 x, 순서 o 나열하기
+//완전탐색, 백트래킹 : 현재 상태에서 가능한 모든 후보군을 따라 들어가며 탐색하는 알고리즘
+	//N개 중 M개를 중복 x, 순서 o 나열하기
+	// 순열의 시간 복잡도 : O(nPm)
 public class Main {
  
-	public static int[] arr;
-	public static boolean[] visit;
-    public static int N,M;
-    public static StringBuilder sb = new StringBuilder();
+	 static int[] arr;
+	 static boolean[] visit;
+     static int N,M;
+	 static StringBuilder sb = new StringBuilder();
  
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		N = in.nextInt();
-		M = in.nextInt();
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
 		// 방법1
 		arr = new int[M];
 		visit = new boolean[N];
-		dfs(0);
+		DFS1(0);
 
 		// 방법2
 		arr = new int [M+1];
 		visit = new boolean[N+1];
-		DFS(1);
+		DFS2(1);
 
         System.out.print(sb.toString());
 	}
  
-	public static void dfs(int depth) {
+	public static void DFS1(int depth) {
 		if (depth == M) {
 			for (int val : arr) {
-				sb.append(val).append(" ");
+				sb.append(val+ " ");
 			}
 			sb.append("\n");
 			return;
@@ -39,20 +43,23 @@ public class Main {
  
 		for (int i = 0; i < N; i++) {
 			if (!visit[i]) {
+
 				visit[i] = true;
+
 				arr[depth] = i + 1;
-				dfs(depth + 1);
+				DFS1(depth + 1);
+
 				visit[i] = false;
 			}
 		}
 	}
 
-	public static void DFS(int depth){
+	public static void DFS2(int depth){
 
 		//종료 조건
 		if( depth == M+1){
 			for(int i =1; i<=M; i++){
-				sb.append(arr[i]).append(" ");
+				sb.append(arr[i]+ " ");
 			}
 			sb.append("\n");
 			return;
@@ -63,8 +70,10 @@ public class Main {
 			if(visit[i]) continue;
 
 			visit[i] = true;
+
 			arr[depth] = i;
-			DFS(depth + 1);
+			DFS2(depth + 1);
+
 			visit[i] = false;
 		}
 
