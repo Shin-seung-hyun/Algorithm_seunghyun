c039c513f11671232babfc210c426b5d68d7869f
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-03-15 15:28:26 +0900

    [Silver III] Title: N과 M (8), Time: 212 ms, Memory: 20784 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -1,42 +1,55 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
+//완전탐색
+    // N개 중 M 개를, 중복 O, 순서 X 고르기 -> nCm
 public class Main {
-	
-	static int N, M;
-	static int[] arr, printArr;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int[] arr, tmp;
+    static boolean[] visit;
+    static int N,M;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N];
-		printArr = new int[M];
-		
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int [N+1];
         st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
-		
-		Arrays.sort(arr);
-		
-		DFS(0, 0);
-	}
-	
-	static void DFS(int startIdx, int depth) {
-        
-		if(depth == M) {
-			for(int i=0; i<M; i++) {
-				System.out.print(printArr[i] + " ");
-			}
-			System.out.println();
-			return;
-		}
-		
-		for(int i=startIdx; i<N; i++) {
-			printArr[depth] = arr[i];
-			DFS(i, depth + 1); 
-		}
-	}
+        for(int i =1; i<=N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr, 1, N+1);
+
+        tmp = new int[M+1];
+        visit = new boolean[N+1];
+
+        DFS(1, 1 );
+
+        System.out.print(sb.toString());
+    }
+
+    static void DFS(int depth , int at){
+
+        //종료 조건
+        if( depth == M+1){
+            for(int i=1; i<=M; i++)
+                sb.append(tmp[i] + " ");
+            sb.append("\n");
+
+            return;
+        }
+
+        // 반복 조건
+        for(int i= at; i<=N; i++){
+
+            tmp[depth] = arr[i];
+            DFS(depth +1, i);
+        }
+    }
+
+
 }
