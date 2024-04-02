8ccd3de1dbe61526f78f5afa7eba50d7111f1a89
Shin-seung-hyun <jjack1028@naver.com>
2024-04-02 20:13:23 +0900

    add : 생일  풀이 추가

---
+++ 백준/Silver/5635. 생일/생일_Comparable.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+//구현
+    //Comparable
+class test{
+    static StringBuilder sb = new StringBuilder();
+
+    static class Stu implements Comparable<Stu> {
+        String name;
+        int dd;
+        int mm;
+        int yyyy;
+
+        public Stu(String name, int dd ,int mm, int yyyy){
+            this.name = name;
+            this.dd =dd ;
+            this.mm =mm;
+            this.yyyy = yyyy;
+        }
+
+        @Override
+        public int compareTo(Stu s){
+            if( this.yyyy == s.yyyy){
+                if(this.mm == s.mm)
+                    return this.dd - s.dd;
+                return this.mm - s.mm;
+            }
+            return this.yyyy - s.yyyy;
+        }
+    }
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        Stu[] student = new Stu[N];
+
+        for(int i=1; i<=N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            String nameStr = st.nextToken();
+            int d = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            student[i-1] = new Stu(nameStr, d, m ,y);
+        }
+
+        Arrays.sort(student);
+
+        System.out.println(student[N-1].name);  // 나이가 어린 사람
+        System.out.println(student[0].name);    // 나이가 많은
+    }
+}
