4c19d80daa93873b6c015d4e8d64bfe21f40b4b0
Shin-seung-hyun <jjack1028@naver.com>
2024-04-06 00:06:26 +0900

    refactor : solved.ac 주석 추가

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -20,11 +20,24 @@ class Main{
 
         int delete = (int) Math.round(N * 0.15);
 
-        // ArrayList의 romove(0)은 모든 원소를 앞으로 당겨와서 비효율적임
+/*
+        <ArrayList 사용 시 O(N^2)로 시간초과 발생 >
+             ArrayList의 romove(0)은 모든 원소를 앞으로 당겨온다
+             따라서 시간복잡도가  O(N) * delete = O(N^2)이다.
+
+        <LinekdList 사용 시 O(N)>
+             문제 해결을 위해 LinkedList를 사용한다면 O(N)이 된다.
+*/
 //        for(int i=1; i<=delete; i++){
 //            arrList.remove(0);
 //            arrList.remove( arrList.size()-1);
 //        }
+//
+//        int sum = 0;
+//        for(int i=0; i<arrList.size()-1 ; i++){
+//            sum += arrList.get(0);
+//        }
+//        System.out.println(Math.round( (double)sum / arrList.size()));
 
         int sum = 0;
         for(int i = delete; i<= arrList.size() - 1 - delete; i++){
