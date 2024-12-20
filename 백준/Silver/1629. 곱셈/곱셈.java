479fdc17ff5c32ad7708187a57c935b5215ced53
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-12-20 14:39:54 +0900

    Merge (#19)
    * [Silver IV] Title: 덱, Time: 200 ms, Memory: 18848 KB -BaekjoonHub
    
    * [Silver III] Title: 전쟁 - 땅따먹기, Time: 4840 ms, Memory: 313844 KB -BaekjoonHub
    
    * [Silver I] Title: 곱셈, Time: 100 ms, Memory: 14304 KB -BaekjoonHub
    
    * [Silver V] Title: 재귀함수가 뭔가요?, Time: 108 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,60 +1,49 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-//재귀
-class Main{
-
-    static long A,B,C;
-
-    public static void main(String[] args) throws IOException{
+public class Main {
+    static long c;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        A = Long.parseLong(st.nextToken());
-        B = Long.parseLong(st.nextToken());
-        C = Long.parseLong(st.nextToken());
+        st = new StringTokenizer(br.readLine());
 
-        // POW함수 내 2번 POW함수 불러서 시간 초과 발생 : O(B)
-        //System.out.println(func(B));
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        c = Long.parseLong(st.nextToken());
 
-        // func() 코드를 개선
-        // B가 절반씩 줄어들어서 시간복잡도 : O(logB)
-        System.out.println(POW(A,B,C));
+        long result = cal(a,b);
+
+        System.out.println(result);
     }
 
-    static long POW(long a, long b ,long c){
-
-        if( b ==1) return a % c;
-
-        long temp = POW(a,b/2,c);
+    static long cal(long x, long y ){
 
         /*
             int 범위 : 2^31 -1
             long 범위 : 2^63 -1
 
             즉, temp = 2^31 -1 이면, (2^31-1 * 2^31-1) < 2^63-1 이지만
-               a까지 2^31-1이라면  (2^31-1 * 2^31-1 * 2^31-1) > 2^63-1로 long의 범위를 넘는다.
+            a까지 2^31-1이라면  (2^31-1 * 2^31-1 * 2^31-1) > 2^63-1로 long의 범위를 넘는다.
             따라서  temp * temp % C 를 해야 한다.
         */
-        if( b % 2 == 1) return ((temp * temp)%c * a) % c;
-        else return temp * temp % c;
-    }
 
-    static long func(Long b){
-
-    // 종료 조건
-        if(b <= 1) {
-            return 1;
+        if( y ==1){
+            return  x % c ;
         }
 
-    // 반복 조건
-        // 짝수일 때
-        if(b % 2 ==0){
-            return (long)(Math.pow(A, func(b / 2)) * (Math.pow(A, func(b / 2))  % C));
+        long tmp = cal(x, y/2);
+
+        if( y % 2 ==0){
+            return tmp * tmp % c ;
         }
-        // 홀수일 때 -> 단, (pow * pow * A % C) 가 아니라 ( (pow * pow %C) *A) % C여야 함
         else {
-            return (long)((Math.pow(A, func( b / 2 )) * Math.pow(A, func(b / 2))) % C) * A % C;
+
+            return (tmp * tmp %c)  * cal (x,1) % c;
         }
 
     }
--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -1,10 +1,15 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-//재귀
-class Main{
+public class Main {
 
-    static int N;
+    static int N ;
+    static String[] strArr = {
+            "\"재귀함수가 뭔가요?\"",
+            "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.",
+            "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.",
+            "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\""
+    };
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException{
@@ -13,31 +18,31 @@ class Main{
 
         N = Integer.parseInt(br.readLine());
 
-        sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
-
+        sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.").append("\n");
         func(1, "");
 
-        System.out.println(sb.toString());
+        System.out.print(sb.toString());
     }
 
     static void func(int cnt, String tmp){
 
-        sb.append(tmp + "\"재귀함수가 뭔가요?\"\n");
-
-        // 종료조건
-        if( cnt == N+1){
-            sb.append(tmp +"\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
-            sb.append(tmp + "라고 답변하였지.\n");
+        //종료조건
+        //if( cnt == N+1){
+        if( cnt > N){
+            sb.append(tmp).append("\"재귀함수가 뭔가요?\"").append("\n");
+            sb.append(tmp).append("\"재귀함수는 자기 자신을 호출하는 함수라네\"").append("\n");
+            sb.append(tmp).append("라고 답변하였지.").append("\n");
             return;
         }
 
         //반복 조건
-        sb.append(  tmp + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
-        sb.append(  tmp +  "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
-        sb.append(  tmp +  "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
-        func(cnt+1, tmp + "____");
-        sb.append(tmp + "라고 답변하였지.\n");
-    }
+        for(String s : strArr){
+            sb.append(tmp).append(s).append("\n");
+        }
 
+        func(cnt +1, tmp+"____");
+
+        sb.append(tmp).append("라고 답변하였지.").append("\n");
+    }
 
 }
