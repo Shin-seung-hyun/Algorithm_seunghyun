f50ebfa7b5b2db684516c410b239862218987fdb
Shin-seung-hyun <59863297+Shin-seung-hyun@users.noreply.github.com>
2024-12-27 23:10:40 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,45 +1,43 @@
 import java.util.*;
 import java.io.*;
 
-// 문자열
 class Main{
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-    
-        int cnt =0;
-        int len = str.length();
-        
-        for(int i= 0; i< str.length(); i++){
-            
-            char ch = str.charAt(i);
-            
-            // c= , c-
-            if( ch =='c' && i+1 <len){
-                if( str.charAt(i+1) == '=' || str.charAt(i+1) == '-') i++;
+
+        //방법1
+//        String str = br.readLine();
+//        str = str.replaceAll("c=|c-|dz=|d-|lj|nj|s=|z=" ," ");
+//        System.out.println(str.length());
+
+        //방법2.
+        char[] charArr = br.readLine().toCharArray();
+
+        int sum =0;
+        for(int i= charArr.length-1; i>=0; i--){
+            if(i-1>=0) {
+                if (charArr[i] == '=') {
+
+                    if (i - 2 >= 0 && charArr[i - 1] == 'z' && charArr[i - 2] == 'd') {
+                        i = i - 2;
+                    }
+                    else if (charArr[i - 1] == 'c' || charArr[i - 1] == 's' || charArr[i - 1] == 'z') {
+                        i--;
+                    }
+                } else if (charArr[i] == 'j') {
+                    if (charArr[i - 1] == 'l' || charArr[i - 1] == 'n') {
+                        i--;
+                    }
+                } else if (charArr[i] == '-') {
+                    if (charArr[i - 1] == 'c' || charArr[i - 1] == 'd') {
+                        i--;
+                    }
+                }
             }
-            
-            //dz=, d-
-            else if ( ch == 'd' && i+1 <len){
-                if ( str.charAt(i+1)== '-') i++;
-                else if (i+2 <len && str.charAt(i+1) == 'z' && str.charAt(i+2) == '=' ) i+=2;
-            }
-            
-            //lj, nj
-            else if ( (ch == 'l' || ch == 'n') && i+1 < len){
-                if( str.charAt(i+1) == 'j') i++;
-            }
-            
-            //s=, z=
-            else if ( (ch =='s' || ch =='z') && i +1 < len){
-                if( str.charAt(i+1) == '=') i++;
-            }
-            
-            cnt++;
+            sum++;
         }
 
-        System.out.println(cnt);
+        System.out.println(sum);
     }
 }
